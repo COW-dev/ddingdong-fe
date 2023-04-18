a6d0d5a1dfcb89c224e36dbe5664103669936255
bk <kkbk0077@gmail.com>
2023-04-18 19:24:01 +0900

    chore: 초기 환경 설정

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  printWidth: 80,
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  proseWrap: 'never',
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      spacing: {
+        18: '4.5rem',
+        30: '7.5rem',
+        88: '22rem',
+        104: '26rem',
+        112: '28rem',
+        128: '32rem',
       },
+      colors: {},
     },
   },
-  plugins: [],
-}
+};
