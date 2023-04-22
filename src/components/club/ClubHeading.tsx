0837fe50a90b8636ed354c6b38a611d14ffed2e0
bk <kkbk0077@gmail.com>
2023-04-22 17:15:29 +0900

    feat: 동아리 상세 페이지 헤딩 구현

---
+++ src/components/club/ClubHeading.tsx
@@ -0,0 +1,67 @@
+import Heading from '@/components/common/Heading';
+import { DeptCaptionColorType } from '@/types/type';
+
+const deptCaptionColor: DeptCaptionColorType = {
+  봉사: 'text-pink-500',
+  사회연구: 'text-orange-500',
+  언행예술: 'text-yellow-500',
+  전시창작: 'text-emerald-500',
+  종교: 'text-cyan-500',
+  체육: 'text-blue-500',
+  학술: 'text-purple-500',
+};
+
+type ClubHeadingProps = {
+  name: string;
+  category: string;
+  tag: string;
+};
+
+export default function ClubHeading({ name, category, tag }: ClubHeadingProps) {
+  return (
+    <>
+      <div className="flex flex-col">
+        <Heading>{name}</Heading>
+        <div className="flex items-center">
+          <div
+            className={`rounded-lg text-lg font-semibold md:text-xl ${deptCaptionColor[category]}`}
+          >
+            {category}
+          </div>
+          <div className="px-1 text-lg font-medium text-gray-300 md:text-xl">
+            |
+          </div>
+          <div className="rounded-lg text-lg font-semibold text-gray-500 md:text-xl">
+            {tag}
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 flex h-44 w-full flex-col justify-center rounded-xl border-[1.5px] border-gray-100 bg-[rgb(252,252,252)] p-5 text-base font-semibold md:mt-6 md:h-36 md:justify-center md:p-6 md:text-lg">
+        <div className="flex w-full flex-col md:flex-row">
+          <div className="mb-0.5 w-88">
+            <span className="inline-block w-20 text-gray-500">회장</span>
+            <span>김보겸</span>
+          </div>
+          <div className="mb-0.5">
+            <span className="inline-block w-20 text-gray-500">연락처</span>
+            <span>010-0000-0000</span>
+          </div>
+        </div>
+        <div className="flex w-full flex-col md:flex-row">
+          <div className="mb-0.5 w-88">
+            <span className="inline-block w-20 text-gray-500">동아리방</span>
+            <span>S4019</span>
+          </div>
+          <div className="mb-0.5">
+            <span className="inline-block w-20 text-gray-500">정기모임</span>
+            <span>매주 월요일</span>
+          </div>
+        </div>
+        <div className="w-full">
+          <span className="inline-block w-20 text-gray-500">모집기간</span>
+          <span>4월 1일 ~ 4월 30일</span>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/club/[id]/index.tsx
@@ -0,0 +1,255 @@
+import { GetServerSideProps } from 'next/types';
+import ClubHeading from '@/components/club/ClubHeading';
+
+const clubs = [
+  {
+    id: 1,
+    name: '명지서법',
+    tag: '서예',
+    category: '학술',
+  },
+  {
+    id: 2,
+    name: 'TIME',
+    tag: '영어',
+    category: '학술',
+  },
+  {
+    id: 3,
+    name: 'MIRS',
+    tag: '기업분석',
+    category: '학술',
+  },
+  {
+    id: 4,
+    name: '통해',
+    tag: '통기타',
+    category: '언행예술',
+  },
+  {
+    id: 5,
+    name: '흑풍',
+    tag: '흑인음악',
+    category: '언행예술',
+  },
+  {
+    id: 6,
+    name: '주리랑',
+    tag: '창작음악',
+    category: '언행예술',
+  },
+  {
+    id: 7,
+    name: '화이트홀스',
+    tag: '밴드',
+    category: '언행예술',
+  },
+  {
+    id: 8,
+    name: 'MGH',
+    tag: '댄스',
+    category: '언행예술',
+  },
+  {
+    id: 9,
+    name: '극예술연구회 알',
+    tag: '연극',
+    category: '언행예술',
+  },
+  {
+    id: 10,
+    name: '너나들이',
+    tag: '건축봉사',
+    category: '봉사',
+  },
+  {
+    id: 11,
+    name: 'RCY',
+    tag: '적십자',
+    category: '봉사',
+  },
+  {
+    id: 12,
+    name: 'PTPI',
+    tag: '연합활동',
+    category: '봉사',
+  },
+  {
+    id: 13,
+    name: '키비탄',
+    tag: '아동봉사',
+    category: '봉사',
+  },
+  {
+    id: 14,
+    name: '코아',
+    tag: '광고',
+    category: '전시창작',
+  },
+  {
+    id: 15,
+    name: '디비전',
+    tag: '영상',
+    category: '전시창작',
+  },
+  {
+    id: 16,
+    name: '포토랩',
+    tag: '사진',
+    category: '전시창작',
+  },
+  {
+    id: 17,
+    name: '그림패시만화',
+    tag: '그림',
+    category: '전시창작',
+  },
+  {
+    id: 18,
+    name: '씨네메이션',
+    tag: '서브컬쳐',
+    category: '전시창작',
+  },
+  {
+    id: 19,
+    name: 'C.C.C',
+    tag: '기독교',
+    category: '종교',
+  },
+  {
+    id: 20,
+    name: '실로암',
+    tag: '기독교',
+    category: '종교',
+  },
+  {
+    id: 21,
+    name: 'CFM',
+    tag: '수화찬양',
+    category: '종교',
+  },
+  {
+    id: 22,
+    name: 'UBF',
+    tag: '성경',
+    category: '종교',
+  },
+  {
+    id: 23,
+    name: '대건안드레아',
+    tag: '가톨릭',
+    category: '종교',
+  },
+  {
+    id: 24,
+    name: '콕콕콕',
+    tag: '배드민턴',
+    category: '체육',
+  },
+  {
+    id: 25,
+    name: 'MJTA',
+    tag: '테니스',
+    category: '체육',
+  },
+  {
+    id: 26,
+    name: '바다이야기',
+    tag: '스킨스쿠버',
+    category: '체육',
+  },
+  {
+    id: 27,
+    name: '무릉도원',
+    tag: '검도',
+    category: '체육',
+  },
+  {
+    id: 28,
+    name: '나이너스',
+    tag: '야구',
+    category: '체육',
+  },
+  {
+    id: 29,
+    name: '삼박자',
+    tag: '족구',
+    category: '체육',
+  },
+  {
+    id: 30,
+    name: '굴렁쇠',
+    tag: '자전거',
+    category: '체육',
+  },
+  {
+    id: 31,
+    name: '파인',
+    tag: '농구',
+    category: '체육',
+  },
+  {
+    id: 32,
+    name: 'FC명지',
+    tag: '축구',
+    category: '체육',
+  },
+  {
+    id: 33,
+    name: '오버행',
+    tag: '클라이밍',
+    category: '체육',
+  },
+  {
+    id: 34,
+    name: '명지챌린저스',
+    tag: '챌린지',
+    category: '사회연구',
+  },
+  {
+    id: 35,
+    name: 'SK루키',
+    tag: '소셜벤처',
+    category: '사회연구',
+  },
+  {
+    id: 36,
+    name: '인액터스',
+    tag: '사회문제',
+    category: '사회연구',
+  },
+  {
+    id: 37,
+    name: '농어민후생연구회 흙',
+    tag: '농활',
+    category: '사회연구',
+  },
+  {
+    id: 38,
+    name: '비주얼',
+    tag: '토론',
+    category: '사회연구',
+  },
+];
+
+type ClubDetailProps = {
+  clubId: number;
+};
+
+export default function Index({ clubId }: ClubDetailProps) {
+  const item = clubs[clubId - 1];
+  return (
+    <ClubHeading name={item.name} category={item.category} tag={item.tag} />
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: {
+  query: any;
+}) => {
+  const { id } = context.query;
+  return {
+    props: {
+      clubId: id,
+    },
+  };
+};
