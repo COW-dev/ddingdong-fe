4696509f4a8362238f34ad540f639eda3caa260f
bk <kkbk0077@gmail.com>
2023-04-22 12:27:23 +0900

    feat: 헤딩 컴포넌트 구현

---
+++ src/components/common/Heading.tsx
@@ -0,0 +1,9 @@
+type HeadingProps = {
+  children: string;
+};
+
+export default function Heading({ children }: HeadingProps) {
+  return (
+    <h1 className="mt-7 text-3xl font-bold md:mt-10 md:text-4xl">{children}</h1>
+  );
+}
