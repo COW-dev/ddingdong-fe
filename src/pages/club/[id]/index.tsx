9132b907184c394e4d9a739d76017f660c70beaf
bk <kkbk0077@gmail.com>
2023-04-22 19:39:39 +0900

    design: 기타 수정

--- src/pages/club/[id]/index.tsx
+++ src/pages/club/[id]/index.tsx
@@ -272,7 +272,7 @@ export default function Index({ clubId }: ClubDetailProps) {
         </section>
       </main>
       <div className="fixed bottom-4 left-0 z-10 flex w-full items-center justify-center px-4 lg:hidden">
-        <button className="w-full rounded-xl bg-blue-500 text-base font-bold text-white drop-shadow-xl transition-colors hover:bg-blue-400 md:text-lg">
+        <button className="button-shadow w-full rounded-xl bg-blue-500 text-base font-bold text-white transition-colors hover:bg-blue-400 md:text-lg">
           <Link href={'/'} className="inline-block w-full py-4">
             지원하기
           </Link>
