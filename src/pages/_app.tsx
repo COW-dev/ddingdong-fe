9bb15e67831a6c2dbca0db9d1532b6f2d48aa958
bk <kkbk0077@gmail.com>
2023-04-22 15:24:14 +0900

    chore: 파비콘, 메타데이터 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,8 +12,11 @@ export default function App({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, user-scalable=no"
         />
         <link rel="icon" href="/favicon.svg" />
-        <meta name="description" content="명지대학교 동아리 정보시스템, 띵동" />
-        <title>띵동 - 명지대 동아리 정보시스템</title>
+        <meta
+          name="description"
+          content="명지대학교의 모든 동아리, 띵동에서 확인해 보세요!"
+        />
+        <title>띵동</title>
       </Head>
       <Layout>
         <Component {...pageProps} />
--- src/pages/notice/index.tsx
+++ src/pages/notice/index.tsx
@@ -1,8 +1,12 @@
 import Heading from '@/components/common/Heading';
+import Head from 'next/head';
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>띵동 - 공지사항</title>
+      </Head>
       <Heading>공지사항</Heading>
     </>
   );
