23522c37fb66cc33600c9720b4db21fb6d64a41f
bk <kkbk0077@gmail.com>
2023-04-24 00:32:49 +0900

    chore: console.log 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const subdomain = req.headers.get('host')?.split('.')[0];
+  console.log(subdomain, req.url);
+
   if (subdomain === 'admin') {
     return NextResponse.rewrite(new URL(`/${subdomain}`, req.url));
   }
