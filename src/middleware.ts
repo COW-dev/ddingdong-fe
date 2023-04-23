5efa93317fe58c6597adb8edc5a89ad4ecef468a
bk <kkbk0077@gmail.com>
2023-04-24 01:02:53 +0900

    fix: 서브도메인 오류 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,12 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const subdomain = req.headers.get('host')?.split('.')[0];
-  console.log(subdomain, req.url);
 
   if (subdomain === 'admin') {
-    return NextResponse.rewrite(new URL('/admin', req.url));
+    return NextResponse.rewrite(new URL('/admin', req.nextUrl.pathname));
   }
 }
 
 export const config = {
-  matcher: '/',
+  matcher: ['/', '/login'],
 };
