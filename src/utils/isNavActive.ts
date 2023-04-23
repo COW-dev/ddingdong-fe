028153b68b102d8038ea0af429abaa4bc2a20549
Bokyeom <79684339+k-kbk@users.noreply.github.com>
2023-04-24 02:26:34 +0900

    refactor: 조건문 로직 수정

--- src/utils/isNavActive.ts
+++ src/utils/isNavActive.ts
@@ -1,12 +1,6 @@
 export default function isNavActive(curPath: string, href: string): boolean {
-  if (curPath.startsWith('/admin')) {
-    if (curPath === '/admin') {
-      return href === '/';
-    }
-    return curPath.startsWith('/admin' + href);
+  if (href === '/') {
+    return curPath === '/' || curPath === '/admin';
   }
-  if (curPath === '/') {
-    return href === '/';
-  }
-  return curPath.startsWith(href);
+  return curPath.startsWith(href) || curPath.startsWith('/admin' + href);
 }
