8e93fa89902ef62d4889e652891ab788e76ab8e2
bk <kkbk0077@gmail.com>
2023-04-24 01:04:06 +0900

    fix: 서브도메인 url 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(req: NextRequest) {
   const subdomain = req.headers.get('host')?.split('.')[0];
 
   if (subdomain === 'admin') {
-    return NextResponse.rewrite(new URL('/admin', req.nextUrl.pathname));
+    return NextResponse.rewrite(new URL('/admin', req.url));
   }
 }
 
