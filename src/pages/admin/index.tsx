357a9d2e2bc7be5aa3b602e21cab25ced9b760a4
bk <kkbk0077@gmail.com>
2023-04-24 20:08:50 +0900

    design: 마진 수정

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -56,20 +56,20 @@ export default function Index() {
       <div className="mt-12 grid w-full max-w-[50rem] grid-cols-1 gap-4 sm:grid-cols-2 md:mt-14 md:gap-8">
         <Link
           href="/clubs"
-          className="inline-block min-h-[8rem] w-full max-w-sm rounded-xl border-[1.5px] p-5 hover:border-gray-200 hover:bg-gray-50 md:min-h-[9.5rem] md:p-8"
+          className="inline-block min-h-[7rem] w-full max-w-sm rounded-xl border-[1.5px] p-5 hover:border-gray-200 hover:bg-gray-50 md:min-h-[8.5rem] md:p-8"
         >
           <h2 className="text-xl font-bold md:text-2xl">동아리 관리하기</h2>
-          <div className="mt-2 text-sm font-semibold leading-tight text-gray-400 md:text-base md:leading-tight">
+          <div className="mt-4 text-sm font-semibold leading-tight text-gray-400 md:text-base md:leading-tight">
             <p>신규 중앙 동아리를 등록하거나,</p>
             <p>기존의 동아리를 삭제해요.</p>
           </div>
         </Link>
         <Link
           href="/reports"
-          className="inline-block min-h-[8rem] w-full max-w-sm rounded-xl border-[1.5px] p-5 hover:border-gray-200 hover:bg-gray-50 md:min-h-[9.5rem] md:p-8"
+          className="inline-block min-h-[7rem] w-full max-w-sm rounded-xl border-[1.5px] p-5 hover:border-gray-200 hover:bg-gray-50 md:min-h-[8.5rem] md:p-8"
         >
           <h2 className="text-xl font-bold md:text-2xl">활동보고서 확인하기</h2>
-          <div className="mt-2 text-sm font-semibold leading-tight text-gray-400 md:text-base md:leading-tight">
+          <div className="mt-4 text-sm font-semibold leading-tight text-gray-400 md:text-base md:leading-tight">
             <p>모든 동아리의 활동보고서를 확인해요.</p>
           </div>
         </Link>
@@ -89,7 +89,7 @@ export default function Index() {
             <li key={item.id} className="w-full border-b">
               <Link
                 href={`/notice/${item.id}`}
-                className="flex flex-col justify-between py-5 text-base font-medium transition-opacity hover:opacity-40 md:flex-row md:items-center md:py-6 md:text-lg"
+                className="flex flex-col justify-between py-5 text-base font-medium transition-opacity hover:opacity-40 md:flex-row md:items-center md:py-5 md:text-lg"
               >
                 <div className="block font-semibold sm:hidden">
                   {item.title.length < 20
