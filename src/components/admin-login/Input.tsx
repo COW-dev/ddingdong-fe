7fedc51cefbcade8c7eda3a279eaa02354788c7f
bk <kkbk0077@gmail.com>
2023-04-24 16:30:18 +0900

    feat: 상태 관리 추가

--- src/components/admin-login/Input.tsx
+++ src/components/admin-login/Input.tsx
@@ -1,10 +1,23 @@
+import { Dispatch, SetStateAction } from 'react';
+
 type InputProps = {
   label: string;
   id: string;
   type: 'text' | 'password';
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
 };
 
-export default function Input({ label, id, type }: InputProps) {
+export default function Input({
+  label,
+  id,
+  type,
+  value,
+  setValue,
+}: InputProps) {
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
   return (
     <div className="mb-4 flex flex-col">
       <label
@@ -17,6 +30,9 @@ export default function Input({ label, id, type }: InputProps) {
         id={id}
         name={id}
         type={type}
+        spellCheck={false}
+        value={value}
+        onChange={handleChange}
         className="rounded-xl border-[1.5px] border-gray-100 bg-gray-50 px-4 py-2 text-base font-medium outline-none sm:text-lg"
       />
     </div>
--- src/pages/admin/login/index.tsx
+++ src/pages/admin/login/index.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Input from '@/components/admin-login/Input';
 import Heading from '@/components/common/Heading';
 
 export default function Index() {
+  const [Id, setId] = useState<string>('');
+  const [Pw, setPw] = useState<string>('');
+
+  function handleSubmit() {
+    return;
+  }
+
   return (
     <>
       <Head>
@@ -11,9 +19,24 @@ export default function Index() {
       <div className="flex w-full flex-col items-center justify-start">
         <div className="mt-10 w-[88%] sm:max-w-md md:w-full">
           <Heading>관리자 로그인</Heading>
-          <form className="mt-12 flex flex-col sm:mt-16">
-            <Input label="아이디" id="id" type="text" />
-            <Input label="비밀번호" id="pw" type="password" />
+          <form
+            className="mt-12 flex flex-col sm:mt-16"
+            onSubmit={handleSubmit}
+          >
+            <Input
+              label="아이디"
+              id="admin-id"
+              type="text"
+              value={Id}
+              setValue={setId}
+            />
+            <Input
+              label="비밀번호"
+              id="admin-pw"
+              type="password"
+              value={Pw}
+              setValue={setPw}
+            />
             <button className="mt-12 w-full rounded-xl bg-blue-500 py-3 font-bold text-white transition-colors hover:bg-blue-600 sm:py-4 sm:text-lg md:mt-16">
               로그인하기
             </button>
