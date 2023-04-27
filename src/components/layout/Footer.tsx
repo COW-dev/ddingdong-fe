7ad658f4b9b72cdaa05b08dd8d4c5c51a3b64db6
bk <kkbk0077@gmail.com>
2023-04-28 01:02:49 +0900

    design: 마진 수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-10 flex h-30 w-full justify-center bg-gray-50 md:mt-20">
+    <footer className="mt-14 flex h-30 w-full justify-center bg-gray-50 md:mt-20">
       <div className="w-full max-w-6xl px-6 md:px-16">
         <div className="mt-12 text-xs font-medium text-gray-500 md:text-sm">
           Copyright ⓒ ddingdong. All Rights Reserved
