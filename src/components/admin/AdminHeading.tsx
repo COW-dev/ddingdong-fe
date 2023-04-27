ea5836248ec225e7bacc3098723fc18086ce4899
bk <kkbk0077@gmail.com>
2023-04-27 19:49:38 +0900

    design: 패딩 수정

--- src/components/admin/AdminHeading.tsx
+++ src/components/admin/AdminHeading.tsx
@@ -1,6 +1,6 @@
 type AdminHeadingProps = {
   clubName: string;
-  clubScore: number;
+  clubScore?: number;
 };
 
 export default function AdminHeading({
@@ -16,14 +16,10 @@ export default function AdminHeading({
         <span className="text-blue-500">{clubName}</span>
         <span className="ml-1 md:ml-1.5">님</span>
       </div>
-      {isClub ? (
-        <div className="rounded-xl bg-sky-100 px-3 py-2 text-lg font-bold text-blue-500 md:text-xl">
+      {isClub && (
+        <div className="rounded-xl bg-sky-100 px-2.5 py-1.5 text-lg font-bold text-blue-500 md:px-3 md:py-2 md:text-xl">
           {clubScore}점
         </div>
-      ) : (
-        <button className="rounded-xl bg-gray-100 px-4 py-2.5 text-base font-bold text-gray-500 hover:bg-gray-200 md:text-base">
-          로그아웃
-        </button>
       )}
     </div>
   );
