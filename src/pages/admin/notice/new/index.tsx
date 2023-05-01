97a9b4ddc7db062c2eedb3b4d25b3e0678fbb602
bk <kkbk0077@gmail.com>
2023-05-02 01:52:00 +0900

    design: 기타 수정

--- src/pages/admin/notice/new/index.tsx
+++ src/pages/admin/notice/new/index.tsx
@@ -12,21 +12,17 @@ export default function Index() {
       <form className="mt-8 w-full md:mt-10">
         <TextareaAutosize
           spellCheck
-          className="w-full resize-none rounded-xl border bg-gray-50 p-5 text-xl font-bold outline-none placeholder:font-medium md:p-6 md:text-2xl"
-          placeholder="제목"
+          className="w-full resize-none border-b py-2 text-xl font-bold outline-none placeholder:font-medium md:py-3 md:text-2xl"
+          placeholder="제목을 입력해주세요"
         />
         <TextareaAutosize
           minRows={8}
           value={content}
           onChange={(event) => setContent(event.target.value)}
           placeholder="내용을 입력해주세요"
-          className="mt-4 h-auto w-full resize-none overflow-hidden rounded-xl border bg-gray-50 p-5 text-base font-medium outline-none md:mt-6 md:p-6 md:text-lg"
+          className="mt-4 h-auto w-full resize-none overflow-hidden rounded-xl border border-gray-100 bg-gray-50 px-5 py-4 text-base font-medium outline-none md:mt-6 md:px-6 md:py-5 md:text-lg"
         />
-        <div className="mt-4 flex justify-end md:mt-6">
-          <button className="w-full rounded-xl bg-blue-500 px-8 py-4 text-base font-bold text-white transition-colors hover:bg-blue-600 md:w-auto md:py-3 md:text-lg">
-            작성하기
-          </button>
-        </div>
+        <div className="mt-4 flex justify-end md:mt-6"></div>
       </form>
     </>
   );
