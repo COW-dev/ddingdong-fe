110f8122d4cabf26bcd586d7f0d003028b0501df
bk <kkbk0077@gmail.com>
2023-05-02 14:39:34 +0900

    design: 기타 수정

--- src/pages/admin/notice/new/index.tsx
+++ src/pages/admin/notice/new/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
       <form className="mt-6 w-full md:mt-8">
         <TextareaAutosize
           spellCheck={false}
-          className="w-full resize-none border-b py-2 text-2xl font-bold outline-none placeholder:font-medium md:py-3 md:text-3xl"
+          className="w-full resize-none rounded-none border-b py-2 text-2xl font-bold outline-none placeholder:font-medium md:py-3 md:text-3xl"
           placeholder="제목을 입력해 주세요"
         />
         <TextareaAutosize
@@ -21,7 +21,7 @@ export default function Index() {
           className="mt-4 h-auto w-full resize-none overflow-hidden rounded-xl border p-5 text-base font-medium outline-none md:mt-6 md:p-6 md:text-lg"
         />
         <div className="mt-4 flex justify-end md:mt-6">
-          <button className="w-full rounded-xl bg-blue-100 px-4 py-3.5 font-bold text-blue-500 transition-colors hover:bg-blue-200 md:w-auto md:py-2.5">
+          <button className="w-full rounded-xl bg-blue-100 px-4 py-4 font-bold text-blue-500 transition-colors hover:bg-blue-200 md:w-auto md:py-2.5">
             작성하기
           </button>
         </div>
