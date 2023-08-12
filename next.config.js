b3658438865b1748343afa77e57a57d6d0223587
youkyeong60 <youkyeongs@naver.com>
2023-08-12 22:36:56 +0900

    feat: API URL 설정

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `http://ddingdong-dev.ap-northeast-2.elasticbeanstalk.com/api/:path*`,
+        destination: `${process.env.BASE_URL}:path*`,
       },
     ];
   },
