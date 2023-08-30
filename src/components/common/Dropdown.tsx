39667d09b9a4566976f7be368b4e9d30f1820ee4
keemsebeen <108217858+keemsebin@users.noreply.github.com>
2023-08-30 15:08:28 +0900

    design : dropdown 메뉴바 생성

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -1,72 +1,85 @@
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Dropdown() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+  function openFixZone() {
+    const windowOptions = 'width=650,height=650,scrollbars,resizable=no';
+    const fixZoneWindow = window.open('fixzone', 'fixzone', windowOptions);
+    if (!fixZoneWindow) {
+      alert('팝업 차단이 활성화되어 있습니다. 팝업 차단을 해제해주세요.');
+    }
+  }
   return (
-    <>
-      <button
-        id="dropdownHoverButton"
-        data-dropdown-toggle="dropdownHover"
-        data-dropdown-trigger="hover"
-        className="inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
+  <>
+    <button
+      id="dropdownHoverButton"
+      data-dropdown-toggle="dropdownHover"
+      data-dropdown-trigger="hover"
+      className={` inline-flex items-center text-blue-500 rounded-xl min-w-fit bg-blue-100 px-5 py-2.5 text-center text-sm md:text-md font-bold hover:bg-blue-200 focus:outline-none ${
+        open ? 'active' : ''
+      } `}
+      type="button"
+      onClick={handleOpen}
+    >
+      동아리 관리하기
+      <svg
+        className="ml-2.5 h-2.5 w-2.5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 10 6"
       >
-        Dropdown hover
-        <svg
-          className="ml-2.5 h-2.5 w-2.5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 10 6"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m1 1 4 4 4-4"
-          />
-        </svg>
-      </button>
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="m1 1 4 4 4-4"
+        />
+      </svg>
+    </button>
+    {open && (
       <div
         id="dropdownHover"
-        className="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+        className=' z-20 relative active min-w-fit divide-y divide-gray-100 rounded-lg bg-blue-200 shadow m-auto '
       >
         <ul
-          className="py-2 text-sm text-gray-700 dark:text-gray-200"
+          className=" absolute w-36 right-0 mt-12 z-10 py-2 text-sm text-gray-700 rounded-xl bg-white shadow-lg border-[1px]"
           aria-labelledby="dropdownHoverButton"
         >
           <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            <div
+              onClick={openFixZone}
+              className="block px-4 py-2 hover:bg-blue-100 font-semibold"
             >
-              Dashboard
-            </a>
+              Fix:Zone
+            </div>
           </li>
           <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Settings
-            </a>
+          <Link
+            href="/club/my/score"
+            className="block px-4 py-2 hover:bg-blue-100 font-semibold"
+            // className="text-md whitespace-nowrap rounded-xl px-2 py-1 font-bold text-blue-400 transition-colors hover:scale-110 hover:bg-slate-50 hover:text-blue-500 md:px-4 md:text-lg"
+          >
+            <div>동아리 점수 확인</div>
+          </Link>
           </li>
           <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            <Link
+              href="/member"
+              className="block px-4 py-2 hover:bg-blue-100 font-semibold"
             >
-              Earnings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Sign out
-            </a>
+              동아리원 수정하기
+            </Link>
           </li>
         </ul>
       </div>
-    </>
+    )}
+  </>
   );
 }
