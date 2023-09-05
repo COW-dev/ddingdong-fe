66b47713a7bd3efe95c95348b01dc4336e65f6b9
youkyeong60 <youkyeongs@naver.com>
2023-09-06 07:59:45 +0900

    feat: 메타 태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable.css"
         />
+        <meta
+          http-equiv="content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
       </Head>
       <body>
         <Main />
