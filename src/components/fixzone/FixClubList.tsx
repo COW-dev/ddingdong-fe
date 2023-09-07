c9d577fa529e4c18647584ed582da9b6ba588756
youkyeong60 <youkyeongs@naver.com>
2023-09-07 12:35:14 +0900

    fix: 픽스존 최신순 정렬

--- src/components/fixzone/FixClubList.tsx
+++ src/components/fixzone/FixClubList.tsx
@@ -5,19 +5,18 @@ import FixItem from './FixItem';
 export default function FixClubList() {
   const [{ token }] = useCookies(['token']);
   const { data } = useMyFix(token);
+  const posts = data?.data ?? [];
+  console.log('posts', posts);
 
   return (
     <div>
       <ul className="mt-14 h-[80vh] w-full overflow-y-scroll md:mt-16">
-        {data?.data
-          .splice(0)
-          .reverse()
-          .map((fix, index) => (
-            <div key={`fix__club-${index}`}>
-              <FixItem data={fix} />
-            </div>
-          ))}
-        {data?.data.length === 0 && (
+        {[...posts].reverse().map((fix, index) => (
+          <div key={`fix__club-${index}`}>
+            <FixItem data={fix} />
+          </div>
+        ))}
+        {posts.length === 0 && (
           <li className="mb-2 flex h-20 w-full flex-col items-center justify-center rounded-xl border border-gray-100 pl-4 pt-2 shadow-sm">
             <div className=" text-sm text-gray-500 ">
               픽스존 요청사항이 존재하지 않습니다.
