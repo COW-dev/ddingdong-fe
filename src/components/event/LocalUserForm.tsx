b9e04d8551e0888b6001f44b23079e56b4b77703
keemsebeen <108217858+keemsebin@users.noreply.github.com>
2024-02-17 18:02:32 +0900

    chore : 변수명 수정

--- src/components/event/LocalUserForm.tsx
+++ src/components/event/LocalUserForm.tsx
@@ -30,6 +30,7 @@ export default function LocalUserForm({ closeModal }: Props) {
       [event.target.name]: event.target.value,
     }));
   }
+
   return (
     <div className="mt-3 flex w-full flex-col justify-center ">
       <div className="mb-3 flex w-full flex-row items-center ">
