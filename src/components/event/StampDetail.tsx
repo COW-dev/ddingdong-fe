f97e1a7364ece6fe6d29476cef10ed00de9adaff
keemsebeen <108217858+keemsebin@users.noreply.github.com>
2024-02-25 17:36:22 +0900

    design : 데이터 유무에 따른 벚꽃 이미지 추가

--- src/components/event/StampDetail.tsx
+++ src/components/event/StampDetail.tsx
@@ -8,32 +8,31 @@ type Props = {
 };
 
 export default function StampDetail({ collections }: Props) {
-  const defaltStamp = Flower;
-  const reciveStamp = CherryFlower;
-
+  console.log(collections);
   return (
     <div className="align-items-center mt-2 grid grid-cols-2 justify-items-center md:mt-10 md:grid-cols-5 ">
       {[...Array(10)].map((_, index) => {
         const collection = collections[index];
-        const stamp = collection ? reciveStamp : defaltStamp;
         return (
           <div key={index}>
             <Image
               key={index}
-              src={stamp}
+              src={collection?.stamp ? CherryFlower : Flower}
               width={25}
               height={25}
               alt={`벚꽃 도장 ${index + 1}`}
               className={
                 index % 2 === 0
-                  ? 'mb-10 mt-14 h-20 w-20 md:mb-20 md:mt-0'
+                  ? 'mb-5 mt-14 h-20 w-20 md:mb-20 md:mt-0'
                   : 'h-20 w-20'
               }
             />
             {collection && (
-              <div key={index}>
+              <div
+                key={index}
+                className={index % 2 === 0 ? 'text-center' : ' m-5'}
+              >
                 <span>{collection.stamp}</span>
-                <span>{collection.collectedAt}</span>
               </div>
             )}
           </div>
