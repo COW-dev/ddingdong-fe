053bf4efe5c228b46133a3f34afa2e50cac3b111
yougyung <youkyeongs@naver.com>
2024-02-29 01:06:14 +0900

    동아리 박람회 작업 내역 (#121)
    * feat : 준동아리 ClubCard 위치 변경
    
    * feat : 동아리원 엑셀 업로드 퍼블리싱
    
    * fix : api 수정
    
    * feat : User 정보 localStrorage 저장
    
    * feat : 이벤트 페이지 퍼블리싱
    
    * feat : 이벤트 페이지 도장 조회 api
    
    * feat : Qr 불러오기
    
    * fix : 도장 조회 type 수정
    
    * fix : localstrorage 값 state 저장 수정
    
    * feat : 도장 수집 api 연결
    
    * feat : 동아리 코드 입력 퍼블리싱
    
    * chore : 변수명 수정
    
    * feat: Header 변경
    
    * design : 디자인(컬러) 수정
    
    * chore : type 변수명 수정
    
    * design : 이동 페이지 버튼 위치 변경
    
    * design : Stamp 디자인
    
    * fix: 현재 주차 변경
    
    * refactor: 활동 보고서 구조 변경
    
    * chore: 불필요한 코드 제거
    
    * refactor: modal 수정
    
    * refactor: 필터 기능 수정
    
    * refactor: 필터 구조 변경
    
    * chore: shadcn ui 설치
    
    * refactor: slider 구현
    
    * refactor: auto scroll plugin 추가
    
    * refactor: 폴더 구조 변경
    
    * feat : User 정보 localStrorage 저장
    
    * design : 이동 페이지 버튼 위치 변경
    
    * feat : 동아리 박람회 이벤트 QR 생성, 도장 수집 (#118)
    
    * refactor: 필터 기능 수정
    
    * refactor: 필터 구조 변경
    
    * chore: shadcn ui 설치
    
    * fix: 필터 의존성 수정
    
    * feat : Qr 불러오기
    
    * chore : 변수명 수정
    
    * refactor: 필터 기능 수정
    
    * refactor: 필터 구조 변경
    
    * chore: shadcn ui 설치
    
    * design : 꽃 디자인 수정
    
    * design : 모달 TItle 수정
    
    * design : 꽃잎 추가
    
    * rename: 도장판 컴포넌트 분리
    
    * feat : 납입증명서 이미지 모달 연결
    
    * feat : 납입 증명서 이미지 업로드
    
    * design : 동아리 부스 지도
    
    * design : 꽃 랜덤 회전
    
    * fix : Hydration Error 수정
    
    * design : 데이터 유무에 따른 벚꽃 이미지 추가
    
    * design : 동아리 스탬프 PC버전 디자인 수정
    
    * feat : user value추가
    
    * design : 디테일 수정
    
    * fix : localStorage 정보 받아오는 부분 수정
    
    * feat : 동아리 코드 입력
    
    * feat : 이벤트 응모하기
    
    * design : 도장 받았을 경우 디자인 수정
    
    * design : 단어 변경
    
    * fix: 중복 제거
    
    * fix : 불필요한 코드 제거
    
    * fix: Build Failed 수정
    
    * fix : cn 적용
    
    * feat: 신청 모달 missingData disabled 처리
    
    * fix : 프리티어 적용
    
    * fix : api url 수정
    
    * fix: url 수정
    
    * fix : api 수정
    
    * feat: 응모하기 api연결
    
    * design : 반응형 sm 디자인 수정
    
    * fix : event 페이지 hydratation 에러 수정
    
    * feat: 응모 완료시 apply LocalStorage 생성 후 재응모 불가
    
    * design: PC버전 DrawMessage 글씨 크기 수정
    
    * chore :clg 삭제
    
    ---------
    
    Co-authored-by: youkyeong60 <youkyeongs@naver.com>
    
    ---------
    
    Co-authored-by: keemsebeen <108217858+keemsebin@users.noreply.github.com>

--- src/components/event/StampBoard.tsx
+++ src/components/event/StampBoard.tsx
@@ -27,23 +27,10 @@ export default function StampBoard({ completed, collections }: ColletionProps) {
           style={{
             top: `${top}%`,
             left: `${petalPositionsLeft[index]}%`,
-<<<<<<< HEAD
-<<<<<<< HEAD
             transform: `rotate(${Math.random() * 360}deg)`,
           }}
           className={` absolute h-5 w-5 md:hidden ${
             index % 2 === 0 ? 'h-5 w-5 ' : ' h-3 w-3'
-=======
-          }}
-          className={` absolute h-5 w-5 md:hidden ${
-            index % 2 === 0 ? 'h-5 w-5' : 'h-3 w-3'
->>>>>>> 8e1c768 (design : 꽃잎 추가)
-=======
-            transform: `rotate(${Math.random() * 360}deg)`,
-          }}
-          className={` absolute h-5 w-5 md:hidden ${
-            index % 2 === 0 ? 'h-5 w-5 ' : ' h-3 w-3'
->>>>>>> 974669c (design : 꽃 랜덤 회전)
           }`}
         />
       ))}
