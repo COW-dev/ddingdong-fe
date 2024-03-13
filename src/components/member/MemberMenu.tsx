02a5ebbbf727609788476017ab317131c319db07
yougyung <youkyeongs@naver.com>
2024-03-13 11:35:14 +0900

    feat: 동아리원 수기 수정 기능 복구

---
+++ src/components/member/MemberMenu.tsx
@@ -0,0 +1,110 @@
+import { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import Modal from '@/components/common/Modal';
+import MemberUpload from '@/components/member/MemberUpload';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Position } from '@/constants/text';
+import { useUpdateMembers } from '@/hooks/api/member/useMembers';
+import useModal from '@/hooks/common/useModal';
+import { Member } from '@/types/club';
+
+interface MemberMenuProps {
+  handleEditting: () => void;
+  members: Member[];
+  isEditing: boolean;
+}
+
+function MemberMenu({ handleEditting, members, isEditing }: MemberMenuProps) {
+  const mutation = useUpdateMembers();
+  const [{ token }] = useCookies(['token']);
+
+  function parsePosition() {
+    const parsedMember: Member[] = [];
+    members.map((member) => {
+      parsedMember.push({
+        ...member,
+        position: Position[member.position],
+      });
+    });
+    return parsedMember;
+  }
+
+  function handleSubmit() {
+    const parsedMember = parsePosition();
+    const formData = new FormData();
+    const member = {
+      clubMemberList: parsedMember,
+    };
+    formData.append(
+      'data',
+      new Blob([JSON.stringify(member)], { type: 'application/json' }),
+    );
+    file && formData.append('file', file);
+    formData.append('token', token);
+    handleEditting();
+    return mutation.mutate(formData);
+  }
+
+  const { openModal, visible, closeModal, modalRef } = useModal();
+  const [file, setFile] = useState<File | null>(null);
+
+  const URL =
+    'https://ddingdong-file.s3.ap-northeast-2.amazonaws.com/files/excel/동아리원_명단_수정_양식.xlsx';
+
+  return (
+    <>
+      <div className="flex gap-2">
+        {isEditing ? (
+          <button
+            onClick={handleEditting}
+            className="cursor-pointer rounded-lg bg-green-100 px-4 py-2 text-sm font-bold text-green-500"
+          >
+            취소
+          </button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">Exel</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56 bg-white">
+              <DropdownMenuLabel>Exel로 동아리원 관리</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuCheckboxItem>
+                <a href={URL} download target="_blank">
+                  Excel 양식 다운로드
+                </a>
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem onClick={openModal}>
+                Excel로 업로드
+              </DropdownMenuCheckboxItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+        <button
+          onClick={isEditing ? handleSubmit : handleEditting}
+          className="cursor-pointer rounded-lg bg-green-100 px-4 py-2 text-sm font-bold text-green-500"
+        >
+          {isEditing ? '저장' : '수정'}
+        </button>
+      </div>
+      <Modal
+        visible={visible}
+        modalRef={modalRef}
+        title={'동아리원 엑셀 업로드'}
+        closeModal={closeModal}
+      >
+        <MemberUpload closeModal={closeModal} file={file} setFile={setFile} />
+      </Modal>
+    </>
+  );
+}
+
+export default MemberMenu;
--- src/pages/admin/member/index.tsx
+++ src/pages/admin/member/index.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
-import Modal from '@/components/common/Modal';
+
 import SearchBar from '@/components/home/SearchBar';
 import MemberInfo from '@/components/member/MemberInfo';
-import MemberUpload from '@/components/member/MemberUpload';
-import { Position } from '@/constants/text';
+import MemberMenu from '@/components/member/MemberMenu';
+
 import { useMyClub } from '@/hooks/api/club/useMyClub';
-import { useUpdateMembers } from '@/hooks/api/member/useMembers';
-import useModal from '@/hooks/common/useModal';
 import { Member } from '@/types/club';
+
 const newMember = {
   id: 0,
   name: '',
@@ -19,54 +18,18 @@ const newMember = {
 };
 export default function Index() {
   const [keyword, setKeyword] = useState<string>('');
-  const { openModal, visible, closeModal, modalRef } = useModal();
   const [{ token }] = useCookies(['token']);
-  const url =
-    'https://ddingdong-file.s3.ap-northeast-2.amazonaws.com/files/excel/동아리원_명단_수정_양식.xlsx';
-  const [file, setFile] = useState<File | null>(null);
-
-  const {
-    data: { data },
-  } = useMyClub(token);
-  const mutation = useUpdateMembers();
-
-  const [members, setMembers] = useState<Member[]>([]);
-  const [filteredMembers, setFilteredMembers] = useState<Member[]>([]);
-  console.log(members);
-  const [isEditing, setIsEditing] = useState<boolean>(false);
-
-  function handleOpenModal() {
-    openModal();
-  }
   function handleEditting() {
     setIsEditing(!isEditing);
   }
-  function handleSubmit() {
-    const parsedMember = parsePosition();
-    const formData = new FormData();
-    const member = {
-      clubMemberList: parsedMember,
-    };
-    formData.append(
-      'data',
-      new Blob([JSON.stringify(member)], { type: 'application/json' }),
-    );
-    // formData.append('data', JSON.stringify(member));
-    file && formData.append('file', file);
-    formData.append('token', token);
-    setIsEditing(!isEditing);
-    return mutation.mutate(formData);
-  }
-  function parsePosition() {
-    const parsedMember: Member[] = [];
-    members.map((member) => {
-      parsedMember.push({
-        ...member,
-        position: Position[member.position],
-      });
-    });
-    return parsedMember;
-  }
+  const {
+    data: { data },
+  } = useMyClub(token);
+
+  const [members, setMembers] = useState<Member[]>([]);
+  const [filteredMembers, setFilteredMembers] = useState<Member[]>([]);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
   useEffect(() => {
     setMembers(data?.clubMembers ?? []);
     setFilteredMembers(data?.clubMembers ?? []);
@@ -94,49 +57,21 @@ export default function Index() {
         동아리원 관리하기
       </div>
       <div className="mb-7 flex justify-between">
-        <div className="font-bold md:text-xl ">
+        <div className="font-bold md:text-xl">
           {data?.name}의 동아리원은 총
           <span className="text-blue-500">{members.length}명</span>
           입니다.
         </div>
-        <div>
-          {/* {isEditing && (
-            <button
-              onClick={handleEditting}
-              className="mr-2 rounded-lg bg-gray-100 px-4 py-2 text-sm font-bold text-gray-500 transition-colors hover:bg-gray-200 md:w-auto md:py-2.5"
-            >
-              취소
-            </button>
-          )} */}
-          <a
-            href={url}
-            download
-            target="_blank"
-            className="mr-3 mt-2 cursor-pointer rounded-lg bg-green-100 px-4 py-2.5 text-sm font-bold text-green-500 transition-colors hover:bg-blue-200 md:w-auto md:py-3"
-          >
-            Excel 양식 다운로드
-          </a>
-          <button
-            // onClick={isEditing ? handleSubmit : handleEditting}
-            onClick={openModal}
-            className="mt-2 cursor-pointer rounded-lg bg-green-100 px-4 py-2 text-sm font-bold text-green-500 transition-colors hover:bg-blue-200 md:w-auto md:py-2.5"
-          >
-            Excel로 업로드
-          </button>
+        <div className="flex gap-2">
+          <MemberMenu
+            handleEditting={handleEditting}
+            members={members}
+            isEditing={isEditing}
+          />
         </div>
       </div>
-      <div className=" rounded-xl border-[1.5px] border-gray-100 p-5  ">
-        {/* {isEditing && (
-          <div className="flex justify-end ">
-            <span
-              onClick={() => handleOpenModal()}
-              className="mt-2 cursor-pointer rounded-lg bg-green-100 px-4 py-2 text-sm font-bold text-green-500 transition-colors hover:bg-blue-200 md:w-auto md:py-2.5"
-            >
-              {' '}
-              Excel로 업로드
-            </span>
-          </div>
-        )} */}
+
+      <div className="rounded-xl border-[1.5px] border-gray-100 p-5  ">
         {!isEditing && <SearchBar value={keyword} onChange={setKeyword} />}
         <div className="mb-3 mt-5 text-sm font-semibold text-gray-500 md:text-base">
           <span className="text-blue-500 opacity-70">
@@ -166,14 +101,6 @@ export default function Index() {
           ))}
         </ul>
       </div>
-      <Modal
-        visible={visible}
-        modalRef={modalRef}
-        title={'동아리원 엑셀 업로드'}
-        closeModal={closeModal}
-      >
-        <MemberUpload closeModal={closeModal} file={file} setFile={setFile} />
-      </Modal>
     </div>
   );
 }
