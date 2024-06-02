b0b91c712a80e64c33f1c4f1b05828261e2b6382
keemsebeen <sebin0580@gmail.com>
2024-06-03 01:40:54 +0900

    feat : timeout Axios 에러 발생시 네트워크 개선 toast 띄우기

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -51,6 +51,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 3000,
 });
+
 export function removeToken() {
   const cookies = new Cookies();
   cookies.remove('token');
@@ -272,6 +273,7 @@ export async function createReport(formdata: FormData) {
       Authorization: 'Bearer ' + token,
       'Content-Type': 'multipart/form-data',
     },
+    timeout: 2000,
   });
 }
 
@@ -436,18 +438,23 @@ function expirationToken(error: AxiosError<ErrorType>) {
   return Promise.reject(error);
 }
 
-function fullfilledResponse(res: AxiosResponse) {
+function fulfilledResponse(res: AxiosResponse) {
   return res;
 }
 function rejectedResponse(error: AxiosError<ErrorType>) {
   if (
     error.response?.data?.code === 401 &&
     error.response?.data?.message == '유효하지 않은 토큰입니다.'
-  )
+  ) {
     return expirationToken(error);
+  }
+  if (error.code === 'ECONNABORTED') {
+    toast.error('네트워크 환경을 확인해주세요.');
+    return Promise.reject(error);
+  }
 
   Sentry.captureException(error);
   return Promise.reject(error);
 }
 
-api.interceptors.response.use(fullfilledResponse, rejectedResponse);
+api.interceptors.response.use(fulfilledResponse, rejectedResponse);
