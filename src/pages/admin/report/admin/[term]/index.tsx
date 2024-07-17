5007ea2160b15a9ef16f451cbf1b7d9c6e95d333
yougyung <youkyeongs@naver.com>
2024-07-17 23:04:03 +0900

    feat: 제출여부 필터링 구현

--- src/pages/admin/report/admin/[term]/index.tsx
+++ src/pages/admin/report/admin/[term]/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -5,40 +6,57 @@ import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next/types';
 import { useCookies } from 'react-cookie';
 import ArrowImage from '@/assets/leftArrow.svg';
+import FilterOption from '@/components/report/FilterOption';
 import { cn } from '@/components/ui/utils';
 import { deptCaptionColor } from '@/constants/color';
 import { useAdminAllReports } from '@/hooks/api/club/useAdminAllReports';
 import { useAllClubs } from '@/hooks/api/club/useAllClubs';
+import { Club } from '@/types';
+
 type ReportPageProps = {
   term: number;
 };
 
 function Index({ term }: ReportPageProps) {
+  const router = useRouter();
   const [{ token }] = useCookies(['token']);
   const { data: allClub } = useAllClubs();
+  const { data: allReports } = useAdminAllReports(token);
 
-  const router = useRouter();
+  const [filteredClub, setFilteredClub] = useState<Club[] | undefined>(
+    allClub?.data,
+  );
+
+  const submitClubNames = allReports?.data
+    .filter((item) => item.term === String(term))
+    .map((item) => item.name);
+  const submitClubs = allClub?.data?.filter((club) =>
+    submitClubNames?.includes(club.name),
+  );
+  const unSubmitClubs = allClub?.data?.filter(
+    (club) => !submitClubNames?.includes(club.name),
+  );
+
+  const optionValue = {
+    all: allClub?.data,
+    submit: submitClubs,
+    unSubmit: unSubmitClubs,
+  };
+
+  const isSubmitClub = (club: Club) => {
+    return submitClubs?.includes(club);
+  };
 
   const handleClickBackButton = () => {
     router.back();
   };
 
-  const clubList = allClub?.data.sort((a, b) =>
-    a.category.localeCompare(b.category),
-  );
-
-  const { data: allReports } = useAdminAllReports(token);
-
-  const submitClubNames = allReports?.data
-    .filter((item) => item.term === String(term))
-    .map((item) => item.name);
-  // const unSubmitClubNames = clubList?.filter(
-  //   (club) => !submitClubNames?.includes(club),
-  // );
-
-  const isSubmitClub = (clubName: string) => {
-    return submitClubNames?.includes(clubName);
-  };
+  useEffect(() => {
+    const sortedClub = allClub?.data.sort((a, b) =>
+      a.category.localeCompare(b.category),
+    );
+    if (sortedClub) setFilteredClub(sortedClub);
+  }, [allClub]);
 
   return (
     <>
@@ -52,9 +70,13 @@ function Index({ term }: ReportPageProps) {
         <Image src={ArrowImage} alt="뒤로가기 화살표" width={40} height={40} />
         {term}회차
       </h1>
-      <div className="mt-12  w-full gap-4 sm:grid-cols-2 md:mt-14 md:gap-8">
+      <FilterOption
+        filterOption={optionValue}
+        setFilteredClub={setFilteredClub}
+      />
+      <div className="mt-6  w-full gap-4 sm:grid-cols-2 md:mt-8 md:gap-8">
         <ul className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:gap-5 lg:grid-cols-3">
-          {clubList?.map((item) => {
+          {filteredClub?.map((item) => {
             return (
               <div
                 key={item.id}
@@ -89,12 +111,12 @@ function Index({ term }: ReportPageProps) {
                     <div
                       className={cn(
                         'flex items-center rounded-lg px-2 py-1 text-sm font-semibold ',
-                        isSubmitClub(item.name)
+                        isSubmitClub(item)
                           ? 'bg-green-50 text-green-400'
                           : 'bg-red-50 text-red-400',
                       )}
                     >
-                      {isSubmitClub(item.name) ? '제출완료' : '미제출'}
+                      {isSubmitClub(item) ? '제출완료' : '미제출'}
                     </div>
                   </div>
                 </Link>
