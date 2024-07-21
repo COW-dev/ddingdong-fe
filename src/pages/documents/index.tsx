94439aaff09695716a81981ee9b97c76228fa791
keemsebeen <sebin0580@gmail.com>
2024-07-21 23:34:15 +0900

    feat: documents 페이지에 modal 연결

--- src/pages/documents/index.tsx
+++ src/pages/documents/index.tsx
@@ -1,8 +1,16 @@
 import Head from 'next/head';
 import DocumentList from '@/components/common/DocumentList';
 import Heading from '@/components/common/Heading';
+import Modal from '@/components/common/Modal';
+import DocumentModal from '@/components/modal/DocumentModal';
+import useModal from '@/hooks/common/useModal';
 
 export default function Index() {
+  const { openModal, visible, closeModal, modalRef } = useModal();
+
+  function handleOpenModal() {
+    openModal();
+  }
   return (
     <>
       <Head>
@@ -10,11 +18,22 @@ export default function Index() {
       </Head>
       <div className=" flex items-end justify-between ">
         <Heading>자료실</Heading>
-        <button className=" ml-3 h-10 cursor-pointer rounded-lg bg-blue-100 px-5 py-2 text-sm font-bold text-blue-500 transition-colors hover:bg-blue-200 md:h-8 md:px-2 md:py-1">
+        <button
+          className=" ] ml-3 h-10 cursor-pointer rounded-lg bg-blue-100 px-4.5 py-2 text-sm font-bold text-blue-500 transition-colors hover:bg-blue-200"
+          onClick={handleOpenModal}
+        >
           업로드
         </button>
       </div>
       <DocumentList />
+      <Modal
+        visible={visible}
+        modalRef={modalRef}
+        title={<span className="text-black">자료실 업로드</span>}
+        closeModal={closeModal}
+      >
+        <DocumentModal closeModal={closeModal} />
+      </Modal>
     </>
   );
 }
