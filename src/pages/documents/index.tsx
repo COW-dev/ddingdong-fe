a304cba0fb18358f062e60afea6025256e6a5e03
keemsebeen <sebin0580@gmail.com>
2024-07-22 20:21:32 +0900

    feat: admin인 경우 업로드 버튼 노출

--- src/pages/documents/index.tsx
+++ src/pages/documents/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head';
+import { useCookies } from 'react-cookie';
 import DocumentList from '@/components/common/DocumentList';
 import Heading from '@/components/common/Heading';
 import Modal from '@/components/common/Modal';
 import DocumentModal from '@/components/modal/DocumentModal';
+import { ROLE_TYPE } from '@/constants/text';
 import useModal from '@/hooks/common/useModal';
 
 export default function Index() {
   const { openModal, visible, closeModal, modalRef } = useModal();
+  const [cookies] = useCookies(['token', 'role']);
+  const { role } = cookies;
 
   function handleOpenModal() {
     openModal();
@@ -19,7 +23,9 @@ export default function Index() {
       <div className=" flex items-end justify-between ">
         <Heading>자료실</Heading>
         <button
-          className=" ] ml-3 h-10 cursor-pointer rounded-lg bg-blue-100 px-4.5 py-2 text-sm font-bold text-blue-500 transition-colors hover:bg-blue-200"
+          className={` ${
+            role === ROLE_TYPE.ROLE_CLUB && 'invisible'
+          } ml-3 h-10 cursor-pointer rounded-lg bg-blue-100 px-4.5 py-2 text-sm font-bold text-blue-500 transition-colors hover:bg-blue-200`}
           onClick={handleOpenModal}
         >
           업로드
