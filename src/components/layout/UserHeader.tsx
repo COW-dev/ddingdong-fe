27bbe323bf972b27785a319a9a82b8283f0759a5
keemsebeen <sebin0580@gmail.com>
2024-08-01 20:21:42 +0900

    feat: 모바일 헤더 구현

--- src/components/layout/UserHeader.tsx
+++ src/components/layout/UserHeader.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import InstaImage from '@/assets/InstaImage.svg';
 import KaKaoImage from '@/assets/kakaoImage.svg';
+import MenuButton from '@/assets/smMenu.svg';
 
 const navItems: {
   [key: string]: {
@@ -54,6 +55,7 @@ const navItems: {
 
 export default function UserHeader() {
   const [hydrated, setHydrated] = useState(false);
+  const [openDrawer, setOpenDrawer] = useState(false);
   const [openDropdown, setOpenDropdown] = useState<string | null>(null);
 
   const handleDropdownToggle = (category: string) => {
@@ -64,15 +66,19 @@ export default function UserHeader() {
     setOpenDropdown(null);
   };
 
+  const handleOpenDrawer = () => {
+    setOpenDrawer(true);
+  };
+
   useEffect(() => {
     setHydrated(true);
   }, []);
   if (!hydrated) return null;
 
-  return (
-    <header className="fixed z-20 flex h-16 w-full items-center justify-center border-b bg-white md:h-18">
-      <div className="flex w-full max-w-6xl items-center px-6 md:px-16">
-        <Link href="/" className="-ml-3 inline-block p-3">
+  const element = () => {
+    return (
+      <div className="flex w-full items-center justify-between px-6">
+        <Link href="/" className="inline-block p-3">
           <Image
             src={'/logo.png'}
             width={1544}
@@ -82,62 +88,98 @@ export default function UserHeader() {
             className="w-30 md:w-34"
           />
         </Link>
-        <ul className="ml-6 flex space-x-8">
-          {Object.keys(navItems).map((category) => (
-            <li key={category} className="relative">
-              {category === '동아리 홍보' ? (
-                <Link
-                  href={navItems[category][0].href}
-                  className="inline-block p-3 font-semibold text-gray-500 transition-colors hover:text-blue-500 focus:outline-none"
-                >
-                  {category}
-                </Link>
-              ) : (
-                <>
-                  <button
-                    onClick={() => handleDropdownToggle(category)}
+        <button className="p-3" onClick={() => handleOpenDrawer()}>
+          <Image
+            src={MenuButton}
+            width={24}
+            height={24}
+            alt="menu"
+            className="h-5"
+          />
+        </button>
+      </div>
+    );
+  };
+  return (
+    <>
+      {/* md */}
+      <header className=" fixed z-20 hidden h-16 w-full items-center justify-center border-b bg-white md:flex md:h-18">
+        <div className="flex w-full max-w-6xl items-center px-6 md:px-16">
+          <Link href="/" className="-ml-3 inline-block p-3 ">
+            <Image
+              src={'/logo.png'}
+              width={1544}
+              height={380}
+              priority
+              alt="ddingdong"
+              className="w-30 md:w-34"
+            />
+          </Link>
+          <ul className="ml-6 flex space-x-8">
+            {Object.keys(navItems).map((category) => (
+              <li key={category} className="relative">
+                {category === '동아리 홍보' ? (
+                  <Link
+                    href={navItems[category][0].href}
                     className="inline-block p-3 font-semibold text-gray-500 transition-colors hover:text-blue-500 focus:outline-none"
                   >
                     {category}
-                  </button>
-                  {openDropdown === category && (
-                    <ul className="absolute left-1/2 top-full mt-2 min-w-max -translate-x-1/2 transform rounded-lg border border-gray-200 bg-white p-3 shadow-lg">
-                      {navItems[category]?.map((item) => (
-                        <li key={item.id}>
-                          <a
-                            href={item.href}
-                            target={category === 'SNS' ? '_blank' : '_self'}
-                            rel={
-                              category === 'SNS'
-                                ? 'noopener noreferrer'
-                                : undefined
-                            }
-                            className={`flex px-3 py-2 font-semibold text-gray-500 hover:rounded-lg hover:bg-gray-100 ${
-                              category === '총동아리 연합회' && 'justify-center'
-                            }`}
-                            onClick={handleLinkClick}
-                          >
-                            {item.image && (
-                              <Image
-                                src={item.image}
-                                width={24}
-                                height={24}
-                                alt="icon"
-                                className="mr-2 h-6 w-6"
-                              />
-                            )}
-                            {item.content}
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </header>
+                  </Link>
+                ) : (
+                  <>
+                    <button
+                      onClick={() => handleDropdownToggle(category)}
+                      className="inline-block p-3 font-semibold text-gray-500 transition-colors hover:text-blue-500 focus:outline-none"
+                    >
+                      {category}
+                    </button>
+                    {openDropdown === category && (
+                      <ul className="absolute left-1/2 top-full mt-2 min-w-max -translate-x-1/2 transform rounded-lg border border-gray-200 bg-white p-3 shadow-lg">
+                        {navItems[category]?.map((item) => (
+                          <li key={item.id}>
+                            <a
+                              href={item.href}
+                              target={category === 'SNS' ? '_blank' : '_self'}
+                              rel={
+                                category === 'SNS'
+                                  ? 'noopener noreferrer'
+                                  : undefined
+                              }
+                              className={`flex px-3 py-2 font-semibold text-gray-500 hover:rounded-lg hover:bg-gray-100 ${
+                                category === '총동아리 연합회' &&
+                                'justify-center'
+                              }`}
+                              onClick={handleLinkClick}
+                            >
+                              {item.image && (
+                                <Image
+                                  src={item.image}
+                                  width={24}
+                                  height={24}
+                                  alt="icon"
+                                  className="mr-2 h-6 w-6"
+                                />
+                              )}
+                              {item.content}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </header>
+      {/* md 끗 */}
+
+      {/* sm */}
+      <header className="fixed z-20 flex h-16 w-full items-center justify-center border-b bg-white md:hidden">
+        {element()}
+      </header>
+      {/* sm 끗 */}
+    </>
   );
 }
