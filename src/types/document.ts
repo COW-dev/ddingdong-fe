e906e56c017186faababab89fa247e500609e974
keemsebeen <sebin0580@gmail.com>
2024-08-12 20:52:43 +0900

    feat: document type 추가

---
+++ src/types/document.ts
@@ -0,0 +1,29 @@
+export type DocumentDetail = {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  fileUrls: { fileUrl: string; name: string }[];
+  imageUrls?: string[];
+};
+
+export type Document = {
+  id: number;
+  title: string;
+  createdAt: string;
+};
+
+export type NewDocument = {
+  title: string;
+  content: string;
+  token: string;
+};
+
+export type UpdateDocument = NewDocument & {
+  documentId: number;
+};
+
+export type DeleteDocument = {
+  documentId: number;
+  token: string;
+};
