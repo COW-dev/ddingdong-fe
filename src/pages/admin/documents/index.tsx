7d728036ed2ae0dd4c9e565250c2d4acda41b162
keemsebeen <sebin0580@gmail.com>
2024-08-17 01:17:56 +0900

    feat: 자료실 페이지 수정/업로드에 따른 로직 추가

--- src/pages/admin/documents/index.tsx
+++ src/pages/admin/documents/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import { useCookies } from 'react-cookie';
 import DocumentList from '@/components/common/DocumentList';
@@ -9,12 +10,15 @@ import useModal from '@/hooks/common/useModal';
 
 export default function Index() {
   const { openModal, visible, closeModal, modalRef } = useModal();
+
+  const [editable, setEditable] = useState<boolean>(false);
   const [cookies] = useCookies(['token', 'role']);
   const { role } = cookies;
 
   function handleOpenModal() {
     openModal();
   }
+
   return (
     <>
       <Head>
@@ -22,16 +26,49 @@ export default function Index() {
       </Head>
       <div className=" flex items-end justify-between ">
         <Heading>자료실</Heading>
-        <button
-          className={` ${
-            role === ROLE_TYPE.ROLE_CLUB && 'invisible'
-          } ml-3 h-10 cursor-pointer rounded-lg bg-blue-100 px-4.5 py-2 text-sm font-bold text-blue-500 transition-colors hover:bg-blue-200`}
-          onClick={handleOpenModal}
-        >
-          업로드
-        </button>
+        <div>
+          {editable ? (
+            <>
+              <button
+                className={` ${
+                  role === ROLE_TYPE.ROLE_CLUB && 'invisible'
+                } ml-3 h-10 cursor-pointer rounded-lg bg-gray-100 px-4.5 py-2 text-sm font-bold text-gray-500 hover:bg-gray-200`}
+                onClick={() => setEditable(false)}
+              >
+                취소
+              </button>
+              <button
+                className={` ${
+                  role === ROLE_TYPE.ROLE_CLUB && 'invisible'
+                } ml-3 h-10 cursor-pointer rounded-lg bg-blue-500 px-4.5 py-2 text-sm font-bold text-white hover:bg-blue-600`}
+                onClick={handleOpenModal}
+              >
+                완료
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className={` ${
+                  role === ROLE_TYPE.ROLE_CLUB && 'invisible'
+                } ml-3 h-10 cursor-pointer rounded-lg bg-blue-100 px-4.5 py-2 text-sm font-bold text-blue-500 transition-colors hover:bg-blue-200`}
+                onClick={() => setEditable(true)}
+              >
+                수정
+              </button>
+              <button
+                className={` ${
+                  role === ROLE_TYPE.ROLE_CLUB && 'invisible'
+                } ml-3 h-10 cursor-pointer rounded-lg bg-blue-100 px-4.5 py-2 text-sm font-bold text-blue-500 transition-colors hover:bg-blue-200`}
+                onClick={handleOpenModal}
+              >
+                업로드
+              </button>
+            </>
+          )}
+        </div>
       </div>
-      <DocumentList />
+      <DocumentList editable={editable} />
       <Modal
         visible={visible}
         modalRef={modalRef}
