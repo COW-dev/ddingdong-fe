e0e3987b732a7269ae23c091e1fbba3ee0cca13c
keemsebeen <sebin0580@gmail.com>
2024-08-21 21:30:59 +0900

    fix: header navItems constants 분리

--- src/components/layout/UserHeader.tsx
+++ src/components/layout/UserHeader.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import InstaImage from '@/assets/InstaImage.svg';
-import KaKaoImage from '@/assets/kakaoImage.svg';
 import MenuButton from '@/assets/smMenu.svg';
 import Drawer from '@/components/common/Drawer';
 import {
@@ -11,54 +9,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
-
-const navItems: {
-  [key: string]: {
-    id: number;
-    href: string;
-    image?: string;
-    content?: string;
-  }[];
-} = {
-  총동아리연합회: [
-    {
-      id: 1,
-      href: '/notice',
-      content: '공지사항',
-    },
-    {
-      id: 2,
-      href: '/documents',
-      content: '자료실',
-    },
-    {
-      id: 3,
-      href: '/FAQ',
-      content: 'FAQ',
-    },
-  ],
-
-  // '동아리 홍보': [
-  //   {
-  //     id: 4,
-  //     href: '/clubs',
-  //   },
-  // ],
-  SNS: [
-    {
-      id: 5,
-      href: 'https://pf.kakao.com/_ExmtkG',
-      image: KaKaoImage,
-      content: '카카오톡',
-    },
-    {
-      id: 6,
-      href: 'https://www.instagram.com/mju_u.th/',
-      image: InstaImage,
-      content: '인스타그램',
-    },
-  ],
-};
+import { navItems } from '@/constants/navItems';
 
 export default function UserHeader() {
   const [hydrated, setHydrated] = useState(false);
