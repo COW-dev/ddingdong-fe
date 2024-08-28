754aa8c358f04b7e43c4194cf8287e2ca965cc9c
keemsebeen <sebin0580@gmail.com>
2024-08-28 21:19:09 +0900

    fix : ScoreList props  type명 history로 변경

--- src/components/score/ScoreList.tsx
+++ src/components/score/ScoreList.tsx
@@ -1,17 +1,17 @@
-import { ScoreDetail } from '@/types/score';
+import { ScoreHistory } from '@/types/score';
 
 type Props = {
-  parseList: ScoreDetail[];
+  history: ScoreHistory[];
 };
 
-export default function ScoreList({ parseList }: Props) {
+export default function ScoreList({ history }: Props) {
   return (
     <div className=" m-auto w-[90%]">
       <label className={`w-20 font-semibold text-gray-500`}>
-        {parseList.length > 0 ? '상세 내역' : '상세 내역이 존재하지 않습니다.'}
+        {history.length > 0 ? '상세 내역' : '상세 내역이 존재하지 않습니다.'}
       </label>
       <div className="my-2 text-gray-600">
-        {parseList.map((data, index) => (
+        {history.map((data, index) => (
           <div
             key={index}
             className=" flex h-10 items-center justify-between space-x-4 border-b"
