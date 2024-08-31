ee5f5a3874d7aea4a9a659be2de5fe109bfd94e3
keemsebeen <sebin0580@gmail.com>
2024-08-31 20:16:25 +0900

    feat : score request enum value 추가

--- src/constants/score.ts
+++ src/constants/score.ts
@@ -6,10 +6,26 @@ import Report from '@/assets/report.svg';
 import Report2 from '@/assets/report2.svg';
 
 export const SCORE_TYPE = {
-  CLEANING: { category: '청소', icon: Clean },
-  ACTIVITY_REPORT: { category: '동아리 활동 보고서', icon: Report },
-  LEADER_CONFERENCE: { category: '전동대회', icon: People },
-  BUSINESS_PARTICIPATION: { category: '총동연 사업 참여', icon: Report2 },
-  ADDITIONAL: { category: '가산점/감점', icon: PlusMinus },
-  CARRYOVER_SCORE: { category: '점수 이월', icon: Dot },
+  CLEANING: { category: '청소', value: 'CLEANING', icon: Clean },
+  ACTIVITY_REPORT: {
+    category: '동아리 활동 보고서',
+    value: 'ACTIVITY_REPORT',
+    icon: Report,
+  },
+  LEADER_CONFERENCE: {
+    category: '전동대회',
+    value: 'LEADER_CONFERENCE',
+    icon: People,
+  },
+  BUSINESS_PARTICIPATION: {
+    category: '총동연 사업 참여',
+    value: 'BUSINESS_PARTICIPATION',
+    icon: Report2,
+  },
+  ADDITIONAL: { category: '가산점/감점', value: 'ADDITIONAL', icon: PlusMinus },
+  CARRYOVER_SCORE: {
+    category: '점수 이월',
+    value: 'CARRYOVER_SCORE',
+    icon: Dot,
+  },
 };
