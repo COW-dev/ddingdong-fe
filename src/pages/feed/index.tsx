0e8552090e6e5a92521ad727798f695f4a5528a8
keemsebeen <sebin0580@gmail.com>
2024-09-07 18:09:35 +0900

    feat : 동아리 피드 페이지 구현

---
+++ src/pages/feed/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import ClubFeed from '@/components/feed/ClubFeed';
+import { useAllFeeds } from '@/hooks/api/feed/useAllFeeds';
+import { Feed } from '@/types/feed';
+
+export default function Index() {
+  const [feeds, setFeeds] = useState<Array<Feed>>([]);
+  const { data } = useAllFeeds();
+
+  useEffect(() => {
+    if (data) {
+      setFeeds(data.data);
+    }
+  }, [data]);
+
+  console.log('data', data);
+  return (
+    <>
+      <Head>
+        <title>띵동 - 동아리피드</title>
+      </Head>
+      <div className="flex w-full items-center justify-center">
+        {feeds.length === 0 ? (
+          <div className="mt-40 text-base font-medium text-gray-500">
+            등록된 게시물이 없습니다.
+          </div>
+        ) : (
+          <div className="w-full">
+            <div className="grid grid-cols-3">
+              <ClubFeed feeds={feeds} />
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
