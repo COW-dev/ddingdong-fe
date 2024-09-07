feaf33f21088785e611d8a34186508502fcb9c1d
keemsebeen <sebin0580@gmail.com>
2024-09-08 00:57:08 +0900

    feat : ClubFeed - ClubFeedDetail 상세조회 Modal 연결

--- src/components/feed/ClubFeed.tsx
+++ src/components/feed/ClubFeed.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import useModal from '@/hooks/common/useModal';
 import { Feed } from '@/types/feed';
+import Modal from '../common/Modal';
 import Skeleton from '../common/Skeleton';
+import ClubFeedDetail from '../modal/feed/ClubFeedDetail';
 
 type ClubFeedProps = {
-  feeds: Feed[] | undefined | null;
+  feeds: Feed[] | null;
+  size: 'medium' | 'large';
 };
 
-export default function ClubFeed({ feeds }: ClubFeedProps) {
+export default function ClubFeed({ feeds, size }: ClubFeedProps) {
   const [loading, setLoading] = useState(true);
+  const [feed, setFeed] = useState<number>(0);
+  const { openModal, visible, closeModal, modalRef } = useModal();
 
   useEffect(() => {
     if (feeds) {
@@ -16,8 +22,13 @@ export default function ClubFeed({ feeds }: ClubFeedProps) {
     }
   }, [feeds]);
 
+  const handleClick = (id: number) => {
+    setFeed(id);
+    openModal();
+  };
+
   return (
-    <div className="flex flex-col md:grid md:grid-cols-3">
+    <div className="grid grid-cols-3">
       {loading
         ? Array(feeds?.length || 1)
             .fill(0)
@@ -27,16 +38,28 @@ export default function ClubFeed({ feeds }: ClubFeedProps) {
               </div>
             ))
         : feeds?.map((item, index) => (
-            <div key={item.id} className=" border">
+            <div key={item.id} className="border">
               <Image
                 src={item.thumbnailUrl}
                 alt={`image-${index + 1}`}
                 width={500}
                 height={500}
-                className="h-60 w-full md:h-50"
+                className={` h-42 w-full ${
+                  size === 'medium' ? 'md:h-54' : ' md:h-60 lg:h-72'
+                }`}
+                onClick={() => handleClick(item.id)}
               />
             </div>
           ))}
+      <Modal
+        visible={visible}
+        modalRef={modalRef}
+        closeModal={closeModal}
+        closeButton={false}
+        feed={true}
+      >
+        <ClubFeedDetail feedId={feed} closeModal={closeModal} />
+      </Modal>
     </div>
   );
 }
