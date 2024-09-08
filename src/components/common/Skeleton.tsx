cf4ab5566f6b2628e3b53ffad960f4057090444b
keemsebeen <sebin0580@gmail.com>
2024-09-09 01:49:21 +0900

    refactor : Skeleton h-full 설정

--- src/components/common/Skeleton.tsx
+++ src/components/common/Skeleton.tsx
@@ -1,5 +1,5 @@
 export default function Skeleton() {
   return (
-    <div className=" h-60 w-full animate-pulse border bg-gray-300 md:h-50"></div>
+    <div className=" h-full w-full animate-pulse border bg-gray-300"></div>
   );
 }
