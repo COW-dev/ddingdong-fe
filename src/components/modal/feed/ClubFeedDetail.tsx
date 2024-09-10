205b9600f6bb038bea35c0897c387e5c552f80de
keemsebeen <sebin0580@gmail.com>
2024-09-10 23:58:02 +0900

    feat : feed 상세 조회 시 동아리 이름 클릭시 상세 조회 페이지로 redirect

--- src/components/modal/feed/ClubFeedDetail.tsx
+++ src/components/modal/feed/ClubFeedDetail.tsx
@@ -3,12 +3,15 @@ import Admin from '@/assets/admin.jpg';
 import VideoPlayer from '@/components/feed/VideoPlayer';
 import { useFeedDetail } from '@/hooks/api/feed/useFeedDetail';
 import { parseImgUrl } from '@/utils/parse';
+import Link from 'next/link';
+import router from 'next/router';
 
 export type Props = {
   feedId: number;
 };
 export default function ClubFeedDetail({ feedId }: Props) {
   const { isError, isSuccess, data } = useFeedDetail(feedId);
+  const location = router.pathname === '/feeds';
 
   if (isError) {
     return <div>에러가 발생했습니다.</div>;
@@ -16,17 +19,18 @@ export default function ClubFeedDetail({ feedId }: Props) {
 
   if (isSuccess) {
     const feed = data.data;
+
     const imageSrc =
-      feed.clubInfo.profileImageUrl.length > 0
-        ? parseImgUrl(feed.clubInfo.profileImageUrl)
+      feed.clubProfile.profileImageUrl.length > 0
+        ? parseImgUrl(feed.clubProfile.profileImageUrl)
         : Admin;
 
     return (
-      <div className="flex h-full w-full flex-col">
+      <div className="flex h-full w-full flex-col ">
         <div
           className={`${
             feed.feedType === 'VIDEO' ? 'h-full ' : 'h-[225px]'
-          }  w-full rounded-t-lg bg-black md:h-[450px]`}
+          } w-full  rounded-t-lg bg-black md:h-[450px]`}
         >
           {feed.feedType === 'VIDEO' ? (
             <VideoPlayer videoUrl={feed.fileUrl} />
@@ -40,7 +44,7 @@ export default function ClubFeedDetail({ feedId }: Props) {
             />
           )}
         </div>
-        <div className=" ml-5 flex h-[17vh] flex-col items-start justify-center md:ml-10 ">
+        <div className="ml-5 flex h-[17vh] flex-col items-start justify-center  md:ml-10 ">
           <div className="flex flex-row items-center justify-center">
             <div className="h-12 w-12 overflow-hidden rounded-full border-[1.5px] border-gray-100 md:h-14 md:w-14">
               <Image
@@ -52,9 +56,12 @@ export default function ClubFeedDetail({ feedId }: Props) {
                 className="m-auto h-12 w-12 rounded-full object-cover md:h-14 md:w-14"
               />
             </div>
-            <div className="ml-2 text-base font-semibold md:text-2xl">
-              {feed.clubInfo.name}
-            </div>
+            <Link
+              href={location ? `/club/${feed.clubProfile.id}` : '#'}
+              className="ml-2 text-base font-semibold md:text-2xl"
+            >
+              {feed.clubProfile.name}
+            </Link>
           </div>
 
           <div className="my-1 text-base font-medium md:text-xl">
