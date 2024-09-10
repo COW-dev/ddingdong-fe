6092606dd112fac708c5fdac602125891c8c75f0
keemsebeen <sebin0580@gmail.com>
2024-09-10 23:59:13 +0900

    feat: feedType이 Video일 경우 비디오 디자인 추가

--- src/components/feed/ClubFeed.tsx
+++ src/components/feed/ClubFeed.tsx
@@ -1,72 +1,69 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import useModal from '@/hooks/common/useModal';
 import { Feed } from '@/types/feed';
 import Modal from '../common/Modal';
 import Skeleton from '../common/Skeleton';
 import ClubFeedDetail from '../modal/feed/ClubFeedDetail';
+import VideoPlayButton from '@/assets/videoplay.svg';
 
 type ClubFeedProps = {
-  feeds: Feed[] | null;
-  size: 'medium' | 'large';
+  feeds: Feed[] | undefined;
 };
 
-export default function ClubFeed({ feeds, size }: ClubFeedProps) {
-  const [loading, setLoading] = useState(true);
+export default function ClubFeed({ feeds }: ClubFeedProps) {
+  const [loadedImages, setLoadedImages] = useState<{ [key: number]: boolean }>(
+    {},
+  );
   const [feedId, setFeedId] = useState<number>(0);
   const { openModal, visible, closeModal, modalRef } = useModal();
-
-  useEffect(() => {
-    if (feeds) {
-      setLoading(false);
-    }
-  }, [feeds]);
 
   const handleClick = (id: number) => {
     setFeedId(id);
     openModal();
   };
 
-  const renderSkeleton = () => (
-    <>
-      {Array(feeds?.length)
-        .fill(0)
-        .map((_, index) => (
-          <div
-            key={index}
-            className={`h-30 w-full gap-1 ${
-              size === 'medium' ? 'md:h-54' : 'md:h-60 lg:h-72'
-            }`}
-          >
-            <Skeleton />
-          </div>
-        ))}
-    </>
-  );
+  const handleImageLoad = (id: number) => {
+    setLoadedImages((prev) => ({ ...prev, [id]: true }));
+  };
 
-  const renderFeeds = () => (
-    <>
-      {feeds?.map((item, index) => (
-        <div key={item.id}>
-          <Image
-            src={item.thumbnailUrl}
-            alt={`image-${index + 1}`}
-            width={500}
-            height={500}
-            style={{ objectFit: 'contain' }}
-            className={`h-30 w-full bg-black ${
-              size === 'medium' ? 'md:h-54' : 'md:h-60 lg:h-72'
-            }`}
-            onClick={() => handleClick(item.id)}
-          />
-        </div>
-      ))}
-    </>
+  const renderSkeleton = () => (
+    <div className="absolute inset-0">
+      <Skeleton />
+    </div>
   );
 
   return (
-    <div className="grid grid-cols-3 gap-1">
-      {loading ? renderSkeleton() : renderFeeds()}
+    <div className="grid grid-cols-3 gap-0.5">
+      {feeds?.map((item, index) => (
+        <div
+          key={item.id}
+          className="relative flex aspect-square w-full cursor-pointer"
+        >
+          {!loadedImages[item.id] && renderSkeleton()}
+          <Image
+            src={item.thumbnailUrl}
+            alt={`image-${index + 1}`}
+            width={350}
+            height={350}
+            onLoad={() => handleImageLoad(item.id)}
+            style={{ objectFit: 'cover' }}
+            className={`aspect-square ${
+              loadedImages[item.id] ? 'visible' : 'invisible'
+            }`}
+            onClick={() => handleClick(item.id)}
+          />
+          {item.feedType == 'VIDEO' && (
+            <Image
+              width={20}
+              height={20}
+              src={VideoPlayButton}
+              alt={'비디오 버튼'}
+              className=" -translate-x-1/5 absolute bottom-1 right-0 shadow-md md:h-12 md:w-12"
+            />
+          )}
+        </div>
+      ))}
       <Modal
         visible={visible}
         modalRef={modalRef}
