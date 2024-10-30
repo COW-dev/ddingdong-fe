879aa86c7d01128f040a60443996486f9bbef5e3
keemsebeen <sebin0580@gmail.com>
2024-10-30 20:34:31 +0900

    feat : AdminHeading data 조회 수정

--- src/components/admin/AdminHeading.tsx
+++ src/components/admin/AdminHeading.tsx
@@ -3,15 +3,13 @@ import { useMyClub } from '@/hooks/api/club/useMyClub';
 
 export default function AdminHeading() {
   const [{ token }] = useCookies(['token']);
-  const {
-    data: { data },
-  } = useMyClub(token);
+  const { data } = useMyClub(token);
 
   return (
     <div className="flex w-full items-end justify-between">
       <div className="mt-7 text-2xl font-bold leading-tight md:mt-10 md:flex md:text-3xl">
         <div className="md:mr-1.5">안녕하세요,</div>
-        <span className="text-blue-500">{data?.name}</span>
+        <span className="text-blue-500">{data?.data.name}</span>
         <span className="ml-1 md:ml-1.5">님</span>
       </div>
     </div>
