a018671aa6398a14d639411b2641a142c3ea7c87
keemsebeen <sebin0580@gmail.com>
2024-10-30 21:05:33 +0900

    fix : Loading 컴포넌트 props명 수정

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -2,10 +2,10 @@ import dynamic from 'next/dynamic';
 import loadingJson from './loading.json';
 
 type Props = {
-  size?: string;
+  className?: string;
 };
 
 const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false });
-export default function Loading({ size = 'w-40' }: Props) {
-  return <Lottie play animationData={loadingJson} className={size} />;
+export default function Loading({ className = 'w-40' }: Props) {
+  return <Lottie play animationData={loadingJson} className={className} />;
 }
