bde47ac037948000067c9a065ceb20447133ddea
yougyung <youkyeongs@naver.com>
2024-10-31 15:17:35 +0900

    refactor: usePresignedUrl 속성 변경

--- src/hooks/common/usePresignedUrl.tsx
+++ src/hooks/common/usePresignedUrl.tsx
@@ -2,23 +2,23 @@ import { useCookies } from 'react-cookie';
 import { toast } from 'react-hot-toast';
 import { getPresignedUrl, uploadPresignedUrl } from '@/apis';
 
-export function usePresignedUrl(fileName: string) {
+export function usePresignedUrl() {
   const [{ token }] = useCookies(['token']);
 
-  const getKey = async (file: File) => {
+  const getId = async (file: File) => {
     try {
-      const { data } = await getPresignedUrl(fileName, token);
-      const { key, contentType, uploadUrl } = data;
+      const { data } = await getPresignedUrl(file.name, token);
+      const { id, contentType, uploadUrl } = data;
 
       await uploadPresignedUrl(uploadUrl, file, contentType);
 
-      return key;
+      return id;
     } catch (error) {
       toast.error(`파일첨부 과정에서 문제가 발생했어요.`);
     }
   };
 
-  const getkeys = async (files: File[]) => {
+  const getIds = async (files: File[]) => {
     try {
       return await Promise.all(files.map((file) => getKey(file)));
     } catch (error) {
@@ -26,5 +26,5 @@ export function usePresignedUrl(fileName: string) {
     }
   };
 
-  return { getKey, getkeys };
+  return { getId, getIds };
 }
