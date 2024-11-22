bd144a56da2878e054755431c0c4b154fc6759b5
keemsebeen <sebin0580@gmail.com>
2024-11-23 03:25:13 +0900

    refactor : 공지사항 등록 presingedUrl 적용

--- src/pages/admin/notice/new/index.tsx
+++ src/pages/admin/notice/new/index.tsx
@@ -2,30 +2,56 @@ import { useState } from 'react';
 import Head from 'next/head';
 import { useCookies } from 'react-cookie';
 import TextareaAutosize from 'react-textarea-autosize';
-import UploadImage from '@/components/common/UploadImage';
 import UploadMultipleFile from '@/components/common/UploadMultipleFiles';
+import UploadMultipleImage from '@/components/common/UploadMultipleImage';
 import { useNewNotice } from '@/hooks/api/notice/useNewNotice';
+import { usePresignedUrl } from '@/hooks/common/usePresignedUrl';
 
 export default function Index() {
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
-  const [file, setFile] = useState<File[]>([]);
-  const [image, setImage] = useState<File | null>(null);
+  const [fileIds, setFileIds] = useState<string[]>([]);
+  const [imageIds, setImageId] = useState<string[]>([]);
   const mutation = useNewNotice();
-  const [cookies] = useCookies(['token']);
+  const [{ token }] = useCookies(['token']);
+  const { getPresignedIds: getImagePresignedId, isLoading: isImageLoading } =
+    usePresignedUrl();
+  const { getPresignedIds: getFilePresignedId, isLoading: isFileLoading } =
+    usePresignedUrl();
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    const formData = new FormData();
-    formData.append('title', title);
-    formData.append('content', content);
-    for (let i = 0; i < file.length; i++) {
-      formData.append('uploadFiles', file[i]);
-    }
-    image && formData.append('thumbnailImages', image);
-    formData.append('token', cookies.token);
-    return mutation.mutate(formData);
+    return mutation.mutate({
+      title,
+      content,
+      fileIds,
+      imageIds,
+      token,
+    });
   }
+
+  const handleClickImage = async (files: File[]) => {
+    const uploadInfo = await getImagePresignedId(files);
+    const uploadIds = uploadInfo.map(({ id }) => id);
+    setImageId((prev) => [...prev, ...uploadIds]);
+    return uploadInfo;
+  };
+
+  const handleClickImageDelete = (index: number) => {
+    setImageId((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  const handleClickFile = async (files: File[]) => {
+    const uploadInfo = await getFilePresignedId(files);
+    const uploadIds = uploadInfo.map(({ id }) => id);
+    setFileIds((prev) => [...prev, ...uploadIds]);
+    return uploadInfo;
+  };
+
+  const handleClickFileDelete = (fileId: string) => {
+    setFileIds((prev) => prev.filter((id) => id !== fileId));
+  };
+
   return (
     <>
       <Head>
@@ -34,7 +60,10 @@ export default function Index() {
       <h1 className="mt-7 text-3xl font-bold md:mt-10 md:text-4xl">
         공지사항 작성하기
       </h1>
-      <form className="mt-12 w-full md:mt-14" onSubmit={handleSubmit}>
+      <form
+        className="mt-12 flex w-full flex-col md:mt-14"
+        onSubmit={handleSubmit}
+      >
         <TextareaAutosize
           value={title}
           spellCheck={false}
@@ -42,7 +71,13 @@ export default function Index() {
           placeholder="제목"
           onChange={(event) => setTitle(event.target.value)}
         />
-        <UploadImage image={image} setImage={setImage} />
+
+        <UploadMultipleImage
+          isLoading={isImageLoading}
+          onDelete={handleClickImageDelete}
+          onAdd={handleClickImage}
+        />
+
         <TextareaAutosize
           minRows={8}
           value={content}
@@ -51,7 +86,11 @@ export default function Index() {
           onChange={(event) => setContent(event.target.value)}
           className="mt-6 h-auto w-full resize-none overflow-hidden rounded-none border-b pb-2 text-base font-medium outline-none placeholder:text-gray-300 md:mt-8 md:pb-3 md:text-lg"
         />
-        <UploadMultipleFile file={file} setFile={setFile} />
+        <UploadMultipleFile
+          isLoading={isFileLoading}
+          onAdd={handleClickFile}
+          onDelete={handleClickFileDelete}
+        />
         <div className="mt-4 flex justify-end md:mt-6">
           <button
             type="submit"
