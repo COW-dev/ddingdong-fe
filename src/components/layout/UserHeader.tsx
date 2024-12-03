4852bcd3e5f17020f3247812a10160452f8f145b
keemsebeen <sebin0580@gmail.com>
2024-12-03 21:27:59 +0900

    style : UserHeader 로고, text color 변경

--- src/components/layout/UserHeader.tsx
+++ src/components/layout/UserHeader.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import MenuButton from '@/assets/smMenu.svg';
+import SnowMan from '@/assets/snowman.svg';
+import Tree from '@/assets/tree.svg';
 import Drawer from '@/components/common/Drawer';
 import {
   Accordion,
@@ -58,25 +60,45 @@ export default function UserHeader() {
   const element = () => {
     return (
       <div className="flex w-full items-center justify-between px-6">
-        <Link href="/" className="inline-block p-3">
+        <Link href="/" className="inline-block pb-2">
           <Image
-            src={'/logo.png'}
+            src={'/christmas_logo.png'}
             width={1544}
             height={380}
             priority
             alt="ddingdong"
-            className="w-30 md:w-34"
+            className="w-32"
           />
         </Link>
-        <button className="p-3" onClick={() => handleOpenDrawer()}>
-          <Image
-            src={MenuButton}
-            width={24}
-            height={24}
-            alt="menu"
-            className="h-5"
-          />
-        </button>
+        <div className="flex items-center space-x-2">
+          <div className="flex w-full items-end">
+            <Image
+              src={SnowMan}
+              width={10}
+              height={10}
+              priority
+              className="w-8"
+              alt="snowman"
+            />
+            <Image
+              src={Tree}
+              width={20}
+              height={20}
+              priority
+              className="w-12 md:w-14"
+              alt="tree"
+            />
+          </div>
+          <button className="p-1" onClick={() => handleOpenDrawer()}>
+            <Image
+              src={MenuButton}
+              width={24}
+              height={24}
+              alt="menu"
+              className="h-5"
+            />
+          </button>
+        </div>
         <Drawer isOpen={isOpen} setIsOpen={setIsOpen}>
           <div className="flex flex-col items-start justify-center p-3">
             {Object.keys(navItems).map((category, index) =>
@@ -104,14 +126,14 @@ export default function UserHeader() {
                     </AccordionTrigger>
                     <AccordionContent>
                       <ul className="flex list-none flex-col space-y-2 pl-0">
-                        {navItems[category]?.map((item, idx) => (
+                        {navItems[category]?.map((item) => (
                           <li key={item.id} className="flex-grow">
                             <Link
                               href={item.href}
                               target={category === 'SNS' ? '_blank' : '_self'}
                               rel="noopener noreferrer"
                               onClick={() => setIsOpen(false)}
-                              className="text-md flex items-center font-semibold text-gray-500 hover:text-blue-500"
+                              className="text-md flex items-center font-semibold text-gray-500 hover:text-christmas-green"
                             >
                               {item.image && (
                                 <Image
@@ -140,69 +162,89 @@ export default function UserHeader() {
   return (
     <>
       {/* md */}
-      <header className=" fixed z-20 hidden h-16 w-full items-center justify-center border-b bg-white md:flex md:h-18">
+      <header className="fixed z-20 hidden h-16 w-full items-center justify-center border-b bg-white md:flex md:h-18">
         <div
           className="flex w-full max-w-6xl items-center px-6 md:px-16"
           ref={dropdownRef}
         >
-          <Link href="/" className="-ml-3 inline-block p-3">
+          <Link href="/" className="-ml-3 inline-block pb-2">
             <Image
-              src={'/logo.png'}
+              src={'/christmas_logo.png'}
               width={1544}
               height={380}
               priority
               alt="ddingdong"
-              className="w-30 md:w-34"
+              className="w-40 md:w-44"
             />
           </Link>
-          <ul className="ml-6 flex space-x-8">
-            {Object.keys(navItems).map((category, index) => (
-              <li key={index} className="relative">
-                {category === '동아리피드' ? (
-                  <Link
-                    href={navItems[category][0].href}
-                    className="inline-block p-3 font-semibold text-gray-500 transition-colors hover:text-blue-500 focus:outline-none"
-                  >
-                    {category}
-                  </Link>
-                ) : (
-                  <div>
-                    <button
-                      onClick={() => handleDropdownToggle(category)}
-                      className="inline-block p-3 font-semibold text-gray-500 transition-colors hover:text-blue-500 focus:outline-none"
+          <div className="flex h-full w-full justify-between pl-6">
+            <ul className="ml-6 flex items-center space-x-8">
+              {Object.keys(navItems).map((category, index) => (
+                <li key={index} className="relative">
+                  {category === '동아리피드' ? (
+                    <Link
+                      href={navItems[category][0].href}
+                      className="inline-block p-3 font-semibold text-gray-500 transition-colors hover:text-christmas-red focus:outline-none"
                     >
                       {category}
-                    </button>
-                    {openDropdown === category && (
-                      <ul className="z-100 absolute left-1/2 top-full mt-2 min-w-max -translate-x-1/2 transform rounded-lg border border-gray-200 bg-white p-3 shadow-lg">
-                        {navItems[category]?.map((item, idx) => (
-                          <li key={idx}>
-                            <Link
-                              href={item.href}
-                              target={category === 'SNS' ? '_blank' : '_self'}
-                              className="flex px-3 py-2 font-semibold text-gray-500 hover:rounded-lg hover:bg-gray-100"
-                              onClick={() => handleLinkClick()}
-                            >
-                              {item.image && (
-                                <Image
-                                  src={item.image}
-                                  width={24}
-                                  height={24}
-                                  alt="icon"
-                                  className="mr-2 h-6 w-6"
-                                />
-                              )}
-                              {item.content}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
+                    </Link>
+                  ) : (
+                    <div>
+                      <button
+                        onClick={() => handleDropdownToggle(category)}
+                        className="inline-block p-3 font-semibold text-gray-500 transition-colors hover:text-christmas-red focus:outline-none"
+                      >
+                        {category}
+                      </button>
+                      {openDropdown === category && (
+                        <ul className="z-100 absolute left-1/2 top-full mt-2 min-w-max -translate-x-1/2 transform rounded-lg border border-gray-200 bg-white p-3 shadow-lg">
+                          {navItems[category]?.map((item, idx) => (
+                            <li key={idx}>
+                              <Link
+                                href={item.href}
+                                target={category === 'SNS' ? '_blank' : '_self'}
+                                className="flex px-3 py-2 font-semibold text-gray-500 hover:rounded-lg hover:bg-gray-100"
+                                onClick={() => handleLinkClick()}
+                              >
+                                {item.image && (
+                                  <Image
+                                    src={item.image}
+                                    width={24}
+                                    height={24}
+                                    alt="icon"
+                                    className="mr-2 h-6 w-6"
+                                  />
+                                )}
+                                {item.content}
+                              </Link>
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+            <div className="flex items-end">
+              <Image
+                src={SnowMan}
+                width={10}
+                height={10}
+                priority
+                className="w-9"
+                alt="snowman"
+              />
+              <Image
+                src={Tree}
+                width={20}
+                height={20}
+                priority
+                className="w-12 md:w-14"
+                alt="tree"
+              />
+            </div>
+          </div>
         </div>
       </header>
       {/* md 끗 */}
