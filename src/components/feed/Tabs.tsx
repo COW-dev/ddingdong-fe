e7dcd1ac46f58612c1be2562004d2d830b19d64b
keemsebeen <sebin0580@gmail.com>
2025-01-19 22:58:35 +0900

    refactor : Tabs 컴포넌트 onTabChange Props 추가, Index number로 변경

--- src/components/feed/Tabs.tsx
+++ src/components/feed/Tabs.tsx
@@ -4,16 +4,17 @@ import { TabMenu } from '@/types/feed';
 type Props = {
   TabMenus: TabMenu[];
   tabContext: 'allClubs' | 'myClub';
+  onTabChange?: (index: number) => void;
 };
-export default function Tabs({ TabMenus, tabContext }: Props) {
-  const [activeTab, setActiveTab] = useState<string>(TabMenus[0].label);
-  const handleTabChange = (tabLabel: string) => {
-    setActiveTab(tabLabel);
+export default function Tabs({ TabMenus, tabContext, onTabChange }: Props) {
+  const [activeTabIndex, setActiveTabIndex] = useState<number>(0);
+  const handleTabChange = (index: number) => {
+    setActiveTabIndex(index);
+    onTabChange?.(index);
   };
 
   const renderContent = () => {
-    const activeMenu = TabMenus.find((menu) => menu.label === activeTab);
-    return activeMenu ? activeMenu.content : null;
+    return TabMenus[activeTabIndex].content;
   };
 
   return (
@@ -32,19 +33,19 @@ export default function Tabs({ TabMenus, tabContext }: Props) {
             className="-mb-px flex items-center justify-between"
             aria-label="Tabs"
           >
-            {TabMenus.map((tab) => (
+            {TabMenus.map((tab, index) => (
               <button
                 key={tab.label}
-                onClick={() => handleTabChange(tab.label)}
+                onClick={() => handleTabChange(index)}
                 className={` w-1/${
                   TabMenus.length
                 } shrink-0 border-b-2 pb-4 text-base font-medium md:text-xl md:font-semibold
                    ${
-                     activeTab === tab.label
+                     activeTabIndex === index
                        ? 'border-blue-500 text-blue-500'
                        : 'border-transparent text-gray-500 hover:border-blue-500 hover:text-blue-500'
                    }`}
-                aria-current={activeTab === tab.label ? 'page' : undefined}
+                aria-current={activeTabIndex === index ? 'page' : undefined}
               >
                 {tab.label}
               </button>
