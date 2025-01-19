f98ba16958278afa820a89b4f3c80c3e9227db8a
keemsebeen <sebin0580@gmail.com>
2025-01-20 01:24:27 +0900

    fix : getMyFeeds api size 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -186,7 +186,7 @@ export async function getMyFeeds(
   currentCursorId: number | -1,
 ): Promise<AxiosResponse<TotalFeed<'clubFeeds'>, unknown>> {
   return await api.get(
-    `/central/my/feeds?currentCursorId=${currentCursorId ?? -1}&size=12`,
+    `/central/my/feeds?currentCursorId=${currentCursorId ?? -1}&size=16`,
     {
       headers: {
         Authorization: 'Bearer ' + token,
