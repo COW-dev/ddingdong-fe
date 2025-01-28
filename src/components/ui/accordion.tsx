1932c9f850732f990e66f09bac73614179310dce
ujinsim <syj11243@naver.com>
2025-01-28 13:28:44 +0900

    feat : FAQ CRUD UI반영

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import * as AccordionPrimitive from '@radix-ui/react-accordion';
-import { ChevronDown } from 'lucide-react';
-
+import { ChevronDown, Trash2 } from 'lucide-react';
 import { cn } from '@/components/ui/utils';
 
 const Accordion = AccordionPrimitive.Root;
-
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
@@ -20,23 +18,30 @@ AccordionItem.displayName = 'AccordionItem';
 
 const AccordionTrigger = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> & {
+    trash?: boolean;
+  }
+>(({ className, children, trash, ...props }, ref) => (
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'text-md flex flex-1 items-start justify-between whitespace-normal break-words p-4 font-semibold transition-all md:my-2 md:items-center md:text-xl [&[data-state=open]>svg]:rotate-180',
+        'text-md flex flex-1 items-start justify-between whitespace-normal break-words p-4 font-semibold transition-all md:my-2 md:items-center md:text-xl',
+        { '[&[data-state=open]>svg]:rotate-180': !trash },
         className,
       )}
       {...props}
     >
       {children}
-      <ChevronDown className="ml-auto flex h-5 w-5 shrink-0  transition-transform duration-200" />
+      {trash ? (
+        <Trash2 className="ml-auto flex h-5 w-5 shrink-0 text-red-400" />
+      ) : (
+        <ChevronDown className="ml-auto flex h-5 w-5 shrink-0 transition-transform duration-200" />
+      )}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
-AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName;
+AccordionTrigger.displayName = 'AccordionTrigger';
 
 const AccordionContent = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Content>,
@@ -44,15 +49,18 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden bg-gray-50 transition-all  data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down md:text-lg"
+    className={cn(
+      'overflow-hidden bg-gray-50 transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down md:text-lg',
+      className,
+    )}
     {...props}
   >
-    <span className={cn('flex items-start whitespace-pre-line p-4', className)}>
+    <div className={cn('flex items-start whitespace-pre-line p-4', className)}>
       {children}
-    </span>
+    </div>
   </AccordionPrimitive.Content>
 ));
 
-AccordionContent.displayName = AccordionPrimitive.Content.displayName;
+AccordionContent.displayName = 'AccordionContent';
 
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
--- src/pages/admin/FAQ/index.tsx
+++ src/pages/admin/FAQ/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import { useCookies } from 'react-cookie';
 import Heading from '@/components/common/Heading';
@@ -10,42 +11,124 @@ import {
 import { QnA } from '@/constants/qna';
 
 export default function Index() {
-  const [cookies] = useCookies(['token', 'role']);
+  const [cookies, setCookie] = useCookies(['token', 'role']);
   const { role } = cookies;
+  const [isEditing, setIsEditing] = useState(false);
+  const [newFAQs, setNewFAQs] = useState([]);
+
+  const addFAQ = () => {
+    const newFAQ = { Q: '질문을 입력해주세요', A: '답변을 입력해주세요' };
+    setNewFAQs([...newFAQs, newFAQ]);
+  };
 
   return (
     <>
       <Head>
         <title>띵동 - FAQ</title>
       </Head>
-      <div className=" flex items-end justify-between ">
-        <Heading>FAQ</Heading>
-        {/* <button
-          className={` ${
-            role === ROLE_TYPE.ROLE_CLUB && 'invisible'
-          } ml-3 h-10 cursor-pointer rounded-lg bg-blue-100 px-4.5 py-2 text-sm font-bold text-blue-500 transition-colors hover:bg-blue-200`}
-        >
-          수정
-        </button> */}
+      <div className="flex items-end justify-between">
+        <Heading>FAQ 관리</Heading>
+        {isEditing ? (
+          <div>
+            <button
+              onClick={() => setIsEditing(false)}
+              className="ml-3 h-10 rounded-lg bg-gray-100 px-4.5 py-2 text-sm font-bold text-gray-500 hover:bg-gray-200"
+            >
+              취소
+            </button>
+            <button
+              onClick={() => setIsEditing(false)}
+              className="ml-3 h-10 rounded-lg bg-blue-500 px-4.5 py-2 text-sm font-bold text-white hover:bg-blue-600"
+            >
+              저장하기
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={() => setIsEditing(true)}
+            className="ml-3 h-10 rounded-lg bg-blue-100 px-4.5 py-2 text-sm font-bold text-blue-500 hover:bg-blue-200"
+          >
+            수정하기
+          </button>
+        )}
       </div>
+      {isEditing && (
+        <div className="flex w-full justify-end pt-10">
+          <div
+            onClick={addFAQ}
+            className="flex w-16 cursor-pointer justify-end border-b-2 border-gray-600 pb-0 font-bold text-gray-600"
+          >
+            FAQ 추가
+          </div>
+        </div>
+      )}
       <div className="mt-10 flex flex-col items-center justify-center">
-        {QnA?.map((item, index) => (
-          <Accordion key={index} type="single" collapsible className="w-full">
-            <AccordionItem value={`item-${index}`}>
-              <AccordionTrigger>
-                <span>Q</span>
-                <span className="text-blue-500">.</span>
-                <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <span className="text-left">{item.Q}</span>
-              </AccordionTrigger>
-              <AccordionContent>
-                <div className="font-bold text-blue-500">A.</div>
-                <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                {item.A}
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        ))}
+        {isEditing ? (
+          <>
+            {newFAQs.map((item, index) => (
+              <Accordion
+                key={index}
+                type="single"
+                collapsible
+                className="w-full"
+              >
+                <AccordionItem value={`item-${index}`}>
+                  <AccordionTrigger trash={true}>
+                    <span>Q</span>
+                    <span className="text-blue-500">.</span>
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <input className="w-full" placeholder={item.Q} />
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="font-bold text-blue-500">A.</div>
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <input placeholder={item.A} className="w-full bg-gray-50" />
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            ))}
+            {QnA.map((item, index) => (
+              <Accordion
+                key={index}
+                type="single"
+                collapsible
+                className="w-full"
+              >
+                <AccordionItem value={`item-${index}`}>
+                  <AccordionTrigger trash={isEditing}>
+                    <span>Q</span>
+                    <span className="text-blue-500">.</span>
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span className="text-left">{item.Q}</span>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="font-bold text-blue-500">A.</div>
+                    <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    {item.A}
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+            ))}
+          </>
+        ) : (
+          QnA.map((item, index) => (
+            <Accordion key={index} type="single" collapsible className="w-full">
+              <AccordionItem value={`item-${index}`}>
+                <AccordionTrigger trash={isEditing}>
+                  <span>Q</span>
+                  <span className="text-blue-500">.</span>
+                  <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                  <span className="text-left">{item.Q}</span>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div className="font-bold text-blue-500">A.</div>
+                  <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                  {item.A}
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          ))
+        )}
       </div>
     </>
   );
