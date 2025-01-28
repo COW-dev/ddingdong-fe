f8744959dbe779a7a6a18f2769b8d5874cf73565
ujinsim <syj11243@naver.com>
2025-01-29 04:47:47 +0900

    feat : FAQ 페이지 CRUD 적용(base)

--- src/pages/admin/FAQ/index.tsx
+++ src/pages/admin/FAQ/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Head from 'next/head';
+import { Trash2 } from 'lucide-react';
 import { useCookies } from 'react-cookie';
+import { deleteFaq } from '@/apis';
 import Heading from '@/components/common/Heading';
 import {
   Accordion,
@@ -8,17 +10,52 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
-import { QnA } from '@/constants/qna';
+import { useAllFaq } from '@/hooks/api/faq/useAllFaq';
+import { useCreateFaq } from '@/hooks/api/faq/useCreateFaq';
+
+interface FAQItem {
+  question: string;
+  reply: string;
+}
 
 export default function Index() {
-  const [cookies, setCookie] = useCookies(['token', 'role']);
-  const { role } = cookies;
+  const [cookies] = useCookies(['token', 'role']);
+  const { token } = cookies;
+
+  const { data: FAQ, isLoading, error } = useAllFaq(token);
+  const { mutate: createFaq, isLoading: isSaving } = useCreateFaq();
+  console.log(FAQ?.data, 'FAQ');
+
   const [isEditing, setIsEditing] = useState(false);
-  const [newFAQs, setNewFAQs] = useState([]);
+  const [newFAQs, setNewFAQs] = useState<FAQItem[]>([]);
+
+  const safeFAQ: FAQItem[] = Array.isArray(FAQ?.data) ? FAQ?.data : [];
 
   const addFAQ = () => {
-    const newFAQ = { Q: '질문을 입력해주세요', A: '답변을 입력해주세요' };
+    const newFAQ: FAQItem = {
+      question: '질문을 입력해주세요',
+      reply: '답변을 입력해주세요',
+    };
     setNewFAQs([...newFAQs, newFAQ]);
+  };
+
+  const saveFAQ = () => {
+    if (newFAQs.length === 0) {
+      return;
+    }
+
+    newFAQs.forEach((faq) => {
+      if (!faq.question || !faq.reply) {
+        alert('질문과 답변 모두 입력부탁');
+        return;
+      }
+
+      createFaq({ token, ...faq });
+    });
+  };
+
+  const isClickedDeleteButton = (questionId: number) => {
+    deleteFaq({ questionId, token });
   };
 
   return (
@@ -37,10 +74,11 @@ export default function Index() {
               취소
             </button>
             <button
-              onClick={() => setIsEditing(false)}
+              onClick={saveFAQ}
               className="ml-3 h-10 rounded-lg bg-blue-500 px-4.5 py-2 text-sm font-bold text-white hover:bg-blue-600"
+              disabled={isSaving}
             >
-              저장하기
+              {isSaving ? '저장 중...' : '저장하기'}
             </button>
           </div>
         ) : (
@@ -63,6 +101,9 @@ export default function Index() {
         </div>
       )}
       <div className="mt-10 flex flex-col items-center justify-center">
+        {safeFAQ.length === 0 && newFAQs.length === 0 && (
+          <div className="text-gray-500">등록된 게시물이 없습니다</div>
+        )}
         {isEditing ? (
           <>
             {newFAQs.map((item, index) => (
@@ -73,21 +114,54 @@ export default function Index() {
                 className="w-full"
               >
                 <AccordionItem value={`item-${index}`}>
-                  <AccordionTrigger trash={true}>
-                    <span>Q</span>
-                    <span className="text-blue-500">.</span>
-                    <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <input className="w-full" placeholder={item.Q} />
+                  <AccordionTrigger isArrow={false}>
+                    <div>
+                      <span>Q</span>
+                      <span className="text-blue-500">.</span>
+                      <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    </div>
+
+                    <input
+                      className="mx-0 w-11/12 pr-10"
+                      placeholder={item.question}
+                      value={item.question}
+                      onChange={(e) =>
+                        setNewFAQs((prev) =>
+                          prev.map((faq, i) =>
+                            i === index
+                              ? { ...faq, question: e.target.value }
+                              : faq,
+                          ),
+                        )
+                      }
+                    />
+                    <Trash2
+                      className=" text-red-400"
+                      onClick={() => isClickedDeleteButton(item.id)}
+                    />
                   </AccordionTrigger>
                   <AccordionContent>
                     <div className="font-bold text-blue-500">A.</div>
                     <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <input placeholder={item.A} className="w-full bg-gray-50" />
+                    <input
+                      placeholder={item.reply}
+                      className="w-full bg-gray-50"
+                      value={item.reply}
+                      onChange={(e) =>
+                        setNewFAQs((prev) =>
+                          prev.map((faq, i) =>
+                            i === index
+                              ? { ...faq, reply: e.target.value }
+                              : faq,
+                          ),
+                        )
+                      }
+                    />
                   </AccordionContent>
                 </AccordionItem>
               </Accordion>
             ))}
-            {QnA.map((item, index) => (
+            {safeFAQ.map((item, index) => (
               <Accordion
                 key={index}
                 type="single"
@@ -95,35 +169,42 @@ export default function Index() {
                 className="w-full"
               >
                 <AccordionItem value={`item-${index}`}>
-                  <AccordionTrigger trash={isEditing}>
-                    <span>Q</span>
-                    <span className="text-blue-500">.</span>
-                    <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <span className="text-left">{item.Q}</span>
+                  <AccordionTrigger isArrow={false}>
+                    <div>
+                      <span>Q</span>
+                      <span className="text-blue-500">.</span>
+                      <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                      <span className="text-left">{item.question}</span>
+                    </div>
+
+                    <Trash2
+                      className="text-red-400"
+                      onClick={() => isClickedDeleteButton(item.id)}
+                    />
                   </AccordionTrigger>
                   <AccordionContent>
                     <div className="font-bold text-blue-500">A.</div>
                     <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    {item.A}
+                    {item.reply}
                   </AccordionContent>
                 </AccordionItem>
               </Accordion>
             ))}
           </>
         ) : (
-          QnA.map((item, index) => (
+          safeFAQ.map((item, index) => (
             <Accordion key={index} type="single" collapsible className="w-full">
               <AccordionItem value={`item-${index}`}>
-                <AccordionTrigger trash={isEditing}>
+                <AccordionTrigger>
                   <span>Q</span>
                   <span className="text-blue-500">.</span>
                   <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                  <span className="text-left">{item.Q}</span>
+                  <span className="text-left">{item.question}</span>
                 </AccordionTrigger>
                 <AccordionContent>
                   <div className="font-bold text-blue-500">A.</div>
                   <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                  {item.A}
+                  {item.reply}
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
