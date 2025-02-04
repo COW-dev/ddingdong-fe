fc1fb92b7420dec69f25cfcef773983c9a52dde6
ujinsim <syj11243@naver.com>
2025-02-04 23:34:25 +0900

    feat : input textArea 설정 추가

--- src/components/form/BaseInput.tsx
+++ src/components/form/BaseInput.tsx
@@ -1,12 +1,15 @@
 export default function BaseInput({
   className = '',
   disabled = false,
+  as = 'input',
   ...props
 }) {
+  const Component = as === 'textarea' ? 'textarea' : 'input';
+
   return (
-    <input
+    <Component
       disabled={disabled}
-      className={`w-[100%] rounded-xl border border-gray-200 bg-gray-50 px-4 py-2.5 font-semibold outline-none md:px-5 ${className}`}
+      className={`w-[100%] resize-none rounded-xl border border-gray-200 bg-gray-50 px-4 py-2.5 font-semibold text-gray-500 outline-none disabled:resize-none md:px-5 ${className}`}
       {...props}
     />
   );
