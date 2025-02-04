8f5b7e0ae464a034785b6af595d55261e66cccbc
ujinsim <syj11243@naver.com>
2025-02-04 23:32:27 +0900

    feat : section info 추가

--- src/components/form/Dropdown.tsx
+++ src/components/form/Dropdown.tsx
@@ -1,21 +1,34 @@
 import { useState } from 'react';
+import Image from 'next/image';
+import ArrowDown from '../../assets/arrowDown.svg';
+
 export default function Dropdown({ contents, selected, setSelected }) {
   const [openDropdown, setOpenDropdown] = useState(false);
 
   return (
-    <div className="relative w-48 font-semibold text-gray-500">
+    <div className="relative w-full font-semibold text-gray-500 md:w-1/3">
       <div
         className="flex cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white px-4 py-3"
         onClick={() => setOpenDropdown(!openDropdown)}
       >
         {selected}
+        <Image
+          src={ArrowDown}
+          alt="arrow"
+          className={`transition-transform duration-300 ${
+            openDropdown ? 'rotate-180' : ''
+          }`}
+        />
       </div>
+
       {openDropdown && (
-        <div className="absolute z-40 mt-1 w-full rounded-lg border border-gray-200 bg-white shadow-lg ">
+        <div className="absolute z-40 mt-1 w-full overflow-hidden rounded-lg border border-gray-200 bg-white shadow-lg">
           {contents.map((item, index) => (
             <div
               key={index}
-              className="cursor-pointer px-4 py-2 hover:bg-gray-100"
+              className={`cursor-pointer px-4 py-3 transition hover:bg-gray-100 ${
+                index === 0 ? 'hover:rounded-t-lg' : ''
+              } ${index === contents.length - 1 ? 'hover:rounded-b-lg' : ''}`}
               onClick={() => {
                 setSelected(item);
                 setOpenDropdown(false);
