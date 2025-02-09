997563446823543eae2df22b37f738bb9c038e28
keemsebeen <sebin0580@gmail.com>
2025-02-10 01:58:00 +0900

    feat: 지원자 개별 조회 훅 구현

---
+++ src/hooks/api/apply/useApplicantInfo.ts
@@ -0,0 +1,21 @@
+/* eslint-disable import/named */
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError, AxiosResponse } from 'axios';
+import { getApplicantInfo } from '@/apis';
+import { ApplicantDetail } from '@/types/apply';
+
+export function useApplicantInfo(
+  formId: number,
+  applicantId: number,
+  token: string,
+) {
+  return useQuery<
+    unknown,
+    AxiosError,
+    AxiosResponse<ApplicantDetail, unknown>,
+    [string, number]
+  >({
+    queryKey: ['applicant-info', applicantId],
+    queryFn: () => getApplicantInfo(formId, applicantId, token),
+  });
+}
