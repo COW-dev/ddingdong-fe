bca0727b0c27b4ab2b77749be0b4bf4538ae7c8d
ujinsim <syj11243@naver.com>
2025-02-15 23:48:29 +0900

    feat : 폼 수정하기 페이지 구현

---
+++ src/pages/admin/apply/[id]/new.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { useCookies } from 'react-cookie';
+import ManageForm from '@/components/apply/ManageForm';
+import Skeleton from '@/components/common/Skeleton';
+import { useAdminForm } from '@/hooks/api/apply/useAdminForm';
+
+export default function Index() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [cookies] = useCookies();
+  const token = cookies.token;
+
+  const { data, isLoading } = useAdminForm(token, Number(id));
+
+  if (isLoading) return <Skeleton />;
+
+  const formData = data?.data || null;
+
+  return (
+    <div>
+      {formData ? <ManageForm formData={formData} id={id} /> : <Skeleton />}
+    </div>
+  );
+}
