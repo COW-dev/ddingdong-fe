c2640b121db883a1729b5bcaf56fc64d9b3552a3
ujinsim <syj11243@naver.com>
2025-02-15 23:44:42 +0900

    types: 지원서 폼 관련 타입 정의 추가

---
+++ src/types/form.ts
@@ -0,0 +1,45 @@
+type Section = string;
+
+export type QuestionType =
+  | 'CHECK_BOX'
+  | 'RADIO'
+  | 'TEXT'
+  | 'LONG_TEXT'
+  | 'FILE';
+
+export interface FormField {
+  question: string;
+  type: QuestionType;
+  options: string[] | [];
+  required: boolean;
+  order: number;
+  section: Section;
+}
+
+export interface FormData {
+  title: string;
+  description?: string | null;
+  startDate: string;
+  endDate: string;
+  hasInterview: boolean;
+  sections: Section[];
+  formFields: FormField[];
+}
+
+export interface ManageFormProps {
+  formData?: FormData;
+}
+
+export interface Answer {
+  fieldId: number;
+  value: string | string[];
+}
+
+export interface ApplyData {
+  name: string;
+  studentNumber: number;
+  department: string;
+  email: string;
+  phoneNumber: string;
+  formAnswers: Answer[];
+}
