12e0f3c431611775caa4058bc0e523f89fc12653
ujinsim <syj11243@naver.com>
2025-02-16 03:01:34 +0900

    Update : formStatus 값 업데이트에 따른 props 변경

--- src/components/apply/FormBlock.tsx
+++ src/components/apply/FormBlock.tsx
@@ -1,18 +1,13 @@
-interface FormBlockProps {
-  title: string;
-  startDate: string;
-  endDate: string;
-  isActive: boolean;
-  onClick: () => void;
-}
+import { FormBlockData } from '@/types/form';
 
 export default function FormBlock({
+  formId,
   title,
   startDate,
   endDate,
-  isActive,
+  formStatus,
   onClick,
-}: FormBlockProps) {
+}: FormBlockData) {
   return (
     <div
       className="flex cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-4"
@@ -25,11 +20,17 @@ export default function FormBlock({
         </p>
       </div>
       <div>
-        {isActive ? (
+        {formStatus == '진행 중' && (
           <span className="rounded px-2 py-1 font-semibold text-green-400">
             진행중
           </span>
-        ) : (
+        )}
+        {formStatus == '진행 전' && (
+          <span className="rounded px-2 py-1 font-semibold text-gray-400">
+            진행전
+          </span>
+        )}
+        {formStatus == '마감' && (
           <span className="rounded px-2 py-1 font-semibold text-red-400">
             종료
           </span>
--- src/types/form.ts
+++ src/types/form.ts
@@ -43,3 +43,12 @@ export interface ApplyData {
   phoneNumber: string;
   formAnswers: Answer[];
 }
+
+export interface FormBlockData {
+  formId: number;
+  title: string;
+  startDate: string;
+  endDate: string;
+  formStatus: '진행 전' | '진행 중' | '마감';
+  onClick: () => void;
+}
