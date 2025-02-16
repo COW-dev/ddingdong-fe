471660ef2fd49539969eae2d4e9e649b0dc88518
ujinsim <syj11243@naver.com>
2025-02-17 06:43:37 +0900

    fix: useAdminForm enabled 추가

--- src/hooks/api/apply/useAdminForm.ts
+++ src/hooks/api/apply/useAdminForm.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getForm } from '@/apis';
 
-export function useAdminForm(token: string, id: number) {
+export function useAdminForm(token: string, id?: number) {
   return useQuery({
     queryKey: ['adminForm', id],
-    queryFn: () => getForm(token, id),
+    queryFn: () => getForm(token, id!),
+    enabled: !!id,
   });
 }
