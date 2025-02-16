99600188ecd66b46530ae1136c5a54f8ba42645d
keemsebeen <sebin0580@gmail.com>
2025-02-16 17:01:42 +0900

    feat: 지원자 메모 저장하기 기능 구현

--- src/pages/admin/apply/[id]/[applicantId]/index.tsx
+++ src/pages/admin/apply/[id]/[applicantId]/index.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import { GetServerSideProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useCookies } from 'react-cookie';
+import TextareaAutosize from 'react-textarea-autosize';
 import LeftArrow from '@/assets/leftArrow2.svg';
 import ApplicantInfo from '@/components/apply/ApplicantInfo';
+import ApplyContentBox from '@/components/apply/ApplyContentBox';
 import ApplyResult from '@/components/apply/ApplyResult';
 import Loading from '@/components/loading/Loading';
 import { STATUS_TYPE } from '@/constants/apply';
 import { useApplicantInfo } from '@/hooks/api/apply/useApplicantInfo';
+import { useUpdateApplicantNote } from '@/hooks/api/apply/useUpdateApplicantNote';
 import { useUpdateApplicantStatus } from '@/hooks/api/apply/useUpdateApplicantStatus';
 import { ApplicantStatus } from '@/types/apply';
 import { getNextStatus } from '@/utils/filter';
@@ -20,8 +24,14 @@ type Props = {
 export default function Index({ id, applicantId }: Props) {
   const [{ token }] = useCookies(['token']);
   const { data: data, isLoading } = useApplicantInfo(id, applicantId, token);
+  const [note, setNote] = useState<string>(data?.data.note || '');
   const applicantData = data?.data;
-  const mutation = useUpdateApplicantStatus();
+  const statusMutation = useUpdateApplicantStatus();
+  const noteMutation = useUpdateApplicantNote(applicantId);
+
+  const handleNoteChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNote(e.target.value);
+  };
 
   if (isLoading)
     return (
@@ -38,6 +48,15 @@ export default function Index({ id, applicantId }: Props) {
     );
   }
 
+  const handleUpdateNote = () => {
+    noteMutation.mutate({
+      formId: id,
+      applicationId: applicantId,
+      note: note,
+      token: token,
+    });
+  };
+
   const handleUpdateStatus = (isPass: boolean) => {
     const nextStatus = getNextStatus(
       applicantData.status as ApplicantStatus,
@@ -45,7 +64,7 @@ export default function Index({ id, applicantId }: Props) {
       isPass,
     );
 
-    mutation.mutate({
+    statusMutation.mutate({
       formId: id,
       applicationIds: [applicantId],
       status: nextStatus,
@@ -67,18 +86,18 @@ export default function Index({ id, applicantId }: Props) {
             <h1 className="ml-3 text-2xl font-bold md:text-4xl">
               {applicantData?.name}
             </h1>
-            <div
+            <span
               className={`${
                 STATUS_TYPE[applicantData.status].backgroundColor
               } ${
                 STATUS_TYPE[applicantData.status].color
-              } ml-2 rounded-lg p-1 text-xs font-semibold md:ml-4 md:px-2 md:py-[5px] md:text-sm`}
+              } ml-2 whitespace-nowrap rounded-lg p-1 text-xs font-semibold md:ml-4 md:px-2 md:py-[5px] md:text-sm`}
             >
               {STATUS_TYPE[applicantData.status].statusText}
-            </div>
+            </span>
           </Link>
         </div>
-        <span className="text-base md:text-lg">
+        <span className="text-end text-sm md:text-lg">
           제출일시{' '}
           {`${
             applicantData?.submittedAt.replaceAll('-', '.').split('T')[0]
@@ -91,6 +110,25 @@ export default function Index({ id, applicantId }: Props) {
         ?.map((answer) => (
           <ApplyResult key={answer.fieldId} {...answer} />
         ))}
+      <ApplyContentBox>
+        <div className="mb-4 text-base font-semibold text-blue-600 md:text-xl md:font-bold">
+          메모
+        </div>
+        <TextareaAutosize
+          defaultValue={data?.data.note}
+          onChange={handleNoteChange}
+          className="w-full resize-none rounded-xl p-[15px] outline outline-gray-300"
+        />
+        <div className="mt-1 flex w-full justify-end">
+          <button
+            type="submit"
+            className="mt-1 justify-end text-base font-semibold text-blue-600 md:text-xl md:font-bold"
+            onClick={handleUpdateNote}
+          >
+            저장하기
+          </button>
+        </div>
+      </ApplyContentBox>
       <div className="mt-3 flex items-center justify-center gap-3 text-lg font-bold">
         <button
           onClick={() => handleUpdateStatus(true)}
