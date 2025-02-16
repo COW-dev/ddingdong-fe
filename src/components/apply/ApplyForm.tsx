81bdb4504342ed0b96195bcaa4700690522aa914
ujinsim <syj11243@naver.com>
2025-02-16 17:25:32 +0900

    feat : 필수 질문 미입력시 제출 비허용

--- src/components/apply/ApplyForm.tsx
+++ src/components/apply/ApplyForm.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import toast from 'react-hot-toast';
 import { useSubmitApply } from '@/hooks/api/apply/useSubmitApply';
 import ApplyContent from './ApplyContent';
 import CommonQuestion from './CommnQuestion';
@@ -8,7 +9,7 @@ interface FormData {
   data: {
     formFields: {
       id: string;
-      type: string;
+      type: 'CHECK_BOX' | 'RADIO' | 'LONG_TEXT' | 'TEXT' | 'FILE';
       options?: string[];
       required: boolean;
       question: string;
@@ -40,8 +41,8 @@ export default function ApplyForm({ formData, setStep }: ApplyFormProps) {
     name: '',
     studentNumber: '',
     department: '',
-    // phoneNumber: '',
-    // email: '',
+    phoneNumber: '',
+    email: '',
   });
 
   const [formAnswers, setFormAnswers] = useState([]);
@@ -50,22 +51,10 @@ export default function ApplyForm({ formData, setStep }: ApplyFormProps) {
     setStep('SECTION');
   };
 
-  // {
-  //   "name": "김띵동",
-  //   "studentNumber": "60200000",
-  //   "department": "융합소프트웨어학부 응용소프트웨어전공",
-  //   "formAnswers": [
-  //     {
-  //       "fieldId": 1,
-  //       "value": [
-  //         "string"
-  //       ]
-  //     }
-  //   ]
-  // }
-
-  const [applyContent, setApplyContent] = useState({});
-  const { mutate, isLoading } = useSubmitApply(id);
+  const [applyContent, setApplyContent] = useState<{ formAnswers: any[] }>({
+    formAnswers: [],
+  });
+  const { mutate, isLoading } = useSubmitApply(Number(id));
 
   useEffect(() => {
     setApplyContent({
@@ -74,11 +63,29 @@ export default function ApplyForm({ formData, setStep }: ApplyFormProps) {
     });
   }, [requiredQuestions, formAnswers]);
 
-  console.log(applyContent, 'applyContent');
   const handleSubmit = () => {
+    const requiredFields = formData.data.formFields.filter(
+      (field) => field.required,
+    );
+
+    for (const field of requiredFields) {
+      const answer = applyContent.formAnswers.find(
+        (ans) => ans.fieldId === field.id,
+      );
+
+      if (
+        !answer ||
+        !answer.value ||
+        (Array.isArray(answer.value) && answer.value.length === 0)
+      ) {
+        toast.error(`"${field.question}" 필수 항목을 입력하여 주세요`);
+        return;
+      }
+    }
+
     mutate(applyContent, {
       onSuccess: () => {
-        setStep('SUBMITED');
+        setStep('SUBMITTED');
       },
     });
   };
@@ -112,13 +119,13 @@ export default function ApplyForm({ formData, setStep }: ApplyFormProps) {
       <div className="flex w-full justify-center gap-3 py-6 font-bold">
         <button
           onClick={goSection}
-          className="rounded-xl bg-gray-100 px-4 py-2 text-gray-500 hover:bg-gray-200"
+          className="rounded-lg bg-gray-100 px-4 py-2 text-gray-500 hover:bg-gray-200"
         >
           취소
         </button>
         <button
           onClick={handleSubmit}
-          className="rounded-xl bg-blue-500 px-12 py-3 text-white hover:bg-blue-600"
+          className="rounded-lg bg-blue-500 px-12 py-3 text-white hover:bg-blue-600"
         >
           제출하기
         </button>
