fdd85e5d0e1cbff16528510557f3e1820acc88aa
yougyung <youkyeongs@naver.com>
2025-02-20 08:28:46 +0900

    fix: textarea -> TextareaAutosize 변경

--- src/components/apply/QuestionSingleContent.tsx
+++ src/components/apply/QuestionSingleContent.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import router from 'next/router';
 import { useCookies } from 'react-cookie';
+import TextareaAutosize from 'react-textarea-autosize';
 import File from '@/assets/file.svg';
 import { AnswerItem, useSingleAnswer } from '@/hooks/api/apply/useSingleAnswer';
-
 const componentMap = {
   TEXT: TextList,
   LONG_TEXT: TextList,
@@ -50,41 +50,16 @@ function FileList({ answer }: { answer: AnswerItem }) {
 }
 
 function TextList({ answer }: { answer: AnswerItem }) {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
-  const resizeTextarea = () => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = 'auto';
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-    }
-  };
-
-  useEffect(() => {
-    resizeTextarea();
-  }, [answer]);
-
-  useEffect(() => {
-    window.addEventListener('resize', resizeTextarea);
-    return () => window.removeEventListener('resize', resizeTextarea);
-  }, []);
-
   const handleClick = () => {
     const { id } = router.query;
     router.push(`/apply/${id}/${answer.applicationId}`);
   };
 
   return (
-    <textarea
-      ref={textareaRef}
-      rows={1}
+    <TextareaAutosize
       onClick={handleClick}
       readOnly
       className="block w-full rounded-xl border border-[#E5E7EB]  p-5 text-sm font-semibold text-[#6B7280] outline-none hover:cursor-pointer hover:border-[#3B82F6] hover:shadow-inner md:text-base"
-      onInput={(e) => {
-        const target = e.target as HTMLTextAreaElement;
-        target.style.height = 'auto';
-        target.style.height = `${target.scrollHeight}px`;
-      }}
       value={answer.answer}
     />
   );
