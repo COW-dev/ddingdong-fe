89236a60dc4c6fcf3f71870cd766d7b71c8dc6f4
ujinsim <syj11243@naver.com>
2025-02-20 20:04:38 +0900

    feat : focus시 border 추가

--- src/components/apply/BaseInput.tsx
+++ src/components/apply/BaseInput.tsx
@@ -33,7 +33,8 @@ export default function BaseInput({
 
       <Component
         disabled={disabled}
-        className={`w-[100%] resize-none rounded-xl border border-gray-200 bg-gray-100 px-4 py-2.5 text-lg font-semibold text-gray-500 outline-none disabled:resize-none md:px-5 ${className}`}
+        className={`w-full resize-none rounded-xl border border-gray-200 bg-gray-100 px-4 py-2.5 text-lg font-semibold text-gray-500 outline-none transition-all duration-300 ease-in-out 
+           focus:border-blue-500 focus:ring-blue-300 disabled:resize-none md:px-5 ${className}`}
         placeholder={
           disabled || as === 'textarea' || as === 'input' ? placeholder : ''
         }
