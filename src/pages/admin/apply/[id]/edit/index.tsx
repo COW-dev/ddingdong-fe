cc0a67ead2cd7f348805ce70df7d6296d09aac7b
ujinsim <syj11243@naver.com>
2025-02-20 18:04:57 +0900

    feat : apply 페이지 연결

--- src/pages/admin/apply/[id]/edit/index.tsx
+++ src/pages/admin/apply/[id]/edit/index.tsx
@@ -1,4 +1,27 @@
-// 지원서 수정 및 마감된 페이지
+import React from 'react';
+import { useRouter } from 'next/router';
+import { useCookies } from 'react-cookie';
+import ManageForm from '@/components/apply/ManageForm';
+import Skeleton from '@/components/common/Skeleton';
+import { useAdminForm } from '@/hooks/api/apply/useAdminForm';
+
 export default function Index() {
-  return <></>;
+  const router = useRouter();
+  const { id } = router.query;
+  const [cookies] = useCookies();
+  const token = cookies.token;
+
+  const { data, isLoading } = useAdminForm(token, Number(id));
+
+  if (isLoading) return <Skeleton />;
+
+  const formData = data?.data || null;
+
+  return (
+    <div>
+      {formData && !isLoading && (
+        <ManageForm formData={formData} id={Number(id)} />
+      )}
+    </div>
+  );
 }
