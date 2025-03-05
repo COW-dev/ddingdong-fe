7dafa68e16236413a51a115544036ad70984aa90
yougyung <youkyeongs@naver.com>
2025-03-05 22:16:38 +0900

    fix: 통계페이지의 다중 파일 노출 지원

--- src/components/apply/QuestionSingleContent.tsx
+++ src/components/apply/QuestionSingleContent.tsx
@@ -38,10 +38,13 @@ export default function QuestionSingleContent({ type, id }: Props) {
   const [{ token }] = useCookies();
 
   const { data } = useSingleAnswer(id, token);
+
   const groupFileItems = (data: AnswerItem[]) => {
     const grouped = data.reduce<Record<number, FileItem>>(
       (acc, { applicationId, name, answer }) => {
-        acc[applicationId] = { applicationId, name, answer: [] };
+        if (!acc[applicationId]) {
+          acc[applicationId] = { applicationId, name, answer: [] };
+        }
         acc[applicationId].answer.push(answer);
         return acc;
       },
@@ -54,7 +57,6 @@ export default function QuestionSingleContent({ type, id }: Props) {
     data?.data.type === 'FILE'
       ? groupFileItems(data?.data.answers)
       : data?.data.answers ?? [];
-
   const isFileItemType = (answer: FileItem | AnswerItem): answer is FileItem =>
     Array.isArray(answer.answer);
 
@@ -96,18 +98,22 @@ function FileList({ answer }: { answer: FileItem }) {
 
   return (
     <label
-      className="flex items-center rounded-xl border border-[#E5E7EB] px-5 py-2 text-sm font-semibold text-[#6B7280] outline-none hover:cursor-pointer hover:border-[#3B82F6] hover:shadow-inner md:text-base"
+      className="flex flex-col rounded-xl border border-[#E5E7EB] px-5 py-2 text-sm font-semibold text-[#6B7280] outline-none hover:cursor-pointer hover:border-[#3B82F6] hover:shadow-inner md:text-base"
       htmlFor="file_input"
       onClick={handleClick}
     >
-      <Image
-        src={File}
-        width={20}
-        height={20}
-        alt="file"
-        className="my-2 cursor-pointer"
-      />
-      <span className="ml-3">{answer.answer}</span>
+      {answer.answer.map((fileName) => (
+        <div key={fileName} className="flex items-center">
+          <Image
+            src={File}
+            width={20}
+            height={20}
+            alt="file"
+            className="my-2 cursor-pointer"
+          />
+          <span className="ml-3">{fileName}</span>
+        </div>
+      ))}
     </label>
   );
 }
