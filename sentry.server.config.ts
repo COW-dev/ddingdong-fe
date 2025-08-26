a3272d690a41e7fd0fc54c10af550545a3a8bb27
keemsebeen <sebin0580@gmail.com>
2025-08-26 18:12:16 +0900

    chore: sentry 설정 수정

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,8 @@
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: 'https://70802b2360dcaf5f64b499d1661927ff@o4506946607579136.ingest.us.sentry.io/4506946608693248',
+  dsn: "https://70802b2360dcaf5f64b499d1661927ff@o4506946607579136.ingest.us.sentry.io/4506946608693248",
   tracesSampleRate: 1,
   debug: false,
+  sendDefaultPii: true,
 });
