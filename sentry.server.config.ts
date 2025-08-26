4bf56f241784b9c0d4dd509977dca8ad7c2228f7
keemsebeen <sebin0580@gmail.com>
2025-08-26 18:29:11 +0900

    chore: sentry 불필요한 설정 제거

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,5 +4,4 @@ Sentry.init({
   dsn: "https://70802b2360dcaf5f64b499d1661927ff@o4506946607579136.ingest.us.sentry.io/4506946608693248",
   tracesSampleRate: 1,
   debug: false,
-  sendDefaultPii: true,
 });
