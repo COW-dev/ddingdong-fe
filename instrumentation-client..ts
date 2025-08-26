55d80e8a1bcbb453deeaf7ef7c23d293a69bd9ad
keemsebeen <sebin0580@gmail.com>
2025-08-26 19:07:24 +0900

    chore: sentry 네임드 임포트 전환

--- instrumentation-client..ts
+++ instrumentation-client..ts
@@ -1,13 +1,13 @@
-import * as Sentry from "@sentry/nextjs";
+import { init, replayIntegration } from "@sentry/nextjs";
 
-Sentry.init({
+init({
   dsn: "https://70802b2360dcaf5f64b499d1661927ff@o4506946607579136.ingest.us.sentry.io/4506946608693248",
   tracesSampleRate: 1,
   debug: false,
   replaysOnErrorSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
   integrations: [
-    Sentry.replayIntegration({
+    replayIntegration({
       maskAllText: true,
       blockAllMedia: true,
     }),
