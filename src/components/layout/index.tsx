c1ef84b306e52c345c2f48294b99be58e89d326b
keemsebeen <sebin0580@gmail.com>
2025-08-31 14:14:06 +0900

    fix: header, footer 를 main 태그에서 제거

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -15,13 +15,13 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <>
+      {isAdminPage ? <AdminHeader /> : <UserHeader />}
       <main className="flex min-h-screen w-full flex-col items-center justify-between bg-white text-gray-800">
-        {isAdminPage ? <AdminHeader /> : <UserHeader />}
         <div className="flex w-full max-w-6xl flex-col px-6 pt-22 md:px-16 md:pt-26">
           {children}
         </div>
-        {!isLoginPage && <Footer />}
       </main>
+      {!isLoginPage && <Footer />}
     </>
   );
 }
