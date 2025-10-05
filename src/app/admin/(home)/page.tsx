b8dd83a764fc1c5cd1f84acf7b2e1c1dda63f3e9
keemsebeen <sebin0580@gmail.com>
2025-10-05 17:16:51 +0900

    fix: 토큰 여부에 따라 login 페이지로 리다이렉트

--- src/app/admin/(home)/page.tsx
+++ src/app/admin/(home)/page.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
+import { useEffect } from 'react';
+
 import { useCookie } from '@/app/_api/useCookie';
 
 import AdminPage from './_pages/Admin';
 
 export default function AdminHomePage() {
   const { cookie } = useCookie();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!cookie.role || !cookie.token) {
+      router.replace('/login');
+    }
+  }, [cookie.role, cookie.token, router]);
 
   return <AdminPage role={cookie.role} />;
 }
