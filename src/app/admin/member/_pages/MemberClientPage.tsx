df5fa21a39e04cecb99dbad87debf6db1387759c
keemsebeen <sebin0580@gmail.com>
2025-10-07 17:37:28 +0900

    feat: MemberAddButton 반응형 추가

--- src/app/admin/member/_pages/MemberClientPage.tsx
+++ src/app/admin/member/_pages/MemberClientPage.tsx
@@ -4,6 +4,7 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import {
   Button,
   Flex,
+  IconButton,
   Title1,
   Title3,
   usePortal,
@@ -32,15 +33,36 @@ export function MemberClientPage() {
           </Title3>
           입니다.
         </Title3>
-        <Flex gap={3}>
-          <Button variant="primary" color="blue" size="md" onClick={openModal}>
-            명단 추가하기
-          </Button>
+        <Flex gap={2} alignItems="center">
+          <MemberAddButton openModal={openModal} />
           <ExcelDropdown />
         </Flex>
       </Flex>
       <MemberCard memberInfo={memberInfo.clubMembers} />
       <AddModal isOpen={isOpen} onClose={closeModal} />
+    </>
+  );
+}
+
+function MemberAddButton({ openModal }: { openModal: VoidFunction }) {
+  return (
+    <>
+      <Button
+        variant="primary"
+        color="blue"
+        size="md"
+        className="hidden sm:block"
+        onClick={openModal}
+      >
+        명단 추가하기
+      </Button>
+      <IconButton
+        iconName="write"
+        color="primary"
+        size={20}
+        onClick={openModal}
+        className="block sm:hidden"
+      />
     </>
   );
 }
