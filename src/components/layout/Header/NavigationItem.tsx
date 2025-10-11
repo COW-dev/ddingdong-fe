72f6685ab20a548e75921b3407d68530782e2e6f
심유진 <syj11243@naver.com>
2025-10-11 17:11:42 +0900

    delete: NavigationItem Button 제거

--- src/components/layout/Header/NavigationItem.tsx
+++ src/components/layout/Header/NavigationItem.tsx
@@ -9,7 +9,7 @@ import { Body3 } from 'ddingdong-design-system';
 import { cn } from '@/lib/utils';
 
 type Props = {
-  href?: string;
+  href: string;
   onClick?: MouseEventHandler<HTMLElement>;
   active?: boolean;
   className?: string;
@@ -29,26 +29,9 @@ export function NavigationItem({
     ? 'text-primary-500'
     : 'text-neutral-500 hover:text-primary-300';
 
-  if (href) {
-    const isExternal = /^https?:\/\//.test(href);
-    return (
-      <Link
-        href={href}
-        onClick={onClick}
-        className={cn(base, color, className)}
-      >
-        <Body3 weight="semibold">{children}</Body3>
-      </Link>
-    );
-  }
-
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      className={cn(base, color, className)}
-    >
+    <Link href={href} onClick={onClick} className={cn(base, color, className)}>
       <Body3 weight="semibold">{children}</Body3>
-    </button>
+    </Link>
   );
 }
