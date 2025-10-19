05c14fbec330792ab8cb31592334586e86bc0737
yougyung <youkyeongs@naver.com>
2025-10-19 12:02:08 +0900

    fix: ReportClientPage migration

---
+++ src/app/admin/report/page.tsx
@@ -0,0 +1,23 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { Metadata } from 'next';
+import { ReportClientPage } from './_pages/ReportClientPage';
+import { reportQueryOptions } from '@/app/_api/queries/report';
+
+export const metadata: Metadata = {
+  title: '띵동 - 활동보고서',
+};
+
+export default async function ReportPage() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(reportQueryOptions.terms());
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ReportClientPage />
+    </HydrationBoundary>
+  );
+}
