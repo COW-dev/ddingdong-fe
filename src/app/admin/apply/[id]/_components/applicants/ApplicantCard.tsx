ddd890cd691a7ee2133a41a82815178e370edc62
keemsebeen <sebin0580@gmail.com>
2025-11-16 00:35:00 +0900

    refactor: Link 태그 제거, 카드의 모든 영역을 클릭 시 이동하도록 수정

--- src/app/admin/apply/[id]/_components/applicants/ApplicantCard.tsx
+++ src/app/admin/apply/[id]/_components/applicants/ApplicantCard.tsx
@@ -8,6 +8,7 @@ import {
   getInterviewStatus,
 } from '@/app/admin/apply/[id]/_utils/filter';
 import { STATUS_TYPE } from '@/app/admin/apply/[id]/_constants/apply';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   data: Applicant;
@@ -16,6 +17,7 @@ type Props = {
   onCheck: (checked: boolean) => void;
 };
 export function ApplicantCard({ data, type, checked, onCheck }: Props) {
+  const router = useRouter();
   const status =
     type === 'DOCUMENT'
       ? getDocumentStatus(data.status)
@@ -29,32 +31,33 @@ export function ApplicantCard({ data, type, checked, onCheck }: Props) {
         onCheckedChange={(checked) => onCheck(checked)}
         className="mt-1 flex size-5 flex-shrink-0 cursor-pointer items-center border-gray-200 md:size-6"
       />
-
-      <Flex className="w-full border-b border-gray-200 bg-white pb-3 transition-colors hover:border-gray-200 md:rounded-xl md:border-[1px] md:p-6 md:hover:bg-gray-50">
-        <Link href={`/apply/${data.formId}/${data.id}`} className="flex w-full">
-          <Flex
-            justifyContent="between"
-            alignItems="center"
-            gap={1}
-            className="w-full"
-          >
-            <Flex dir="col" alignItems="start" gap={1}>
-              <Body1 weight="bold">{data.name}</Body1>
-              <Body3 weight="semibold" className="text-gray-400">
-                {data.studentNumber}
-              </Body3>
-            </Flex>
-            <Badge
-              variant={
-                STATUS_TYPE[status].backgroundColor as
-                  | 'positive'
-                  | 'negative'
-                  | 'neutral'
-              }
-              text={STATUS_TYPE[status].text}
-            />
+      <Flex
+        as="button"
+        className="w-full cursor-pointer border-b border-gray-200 bg-white pb-3 transition-colors hover:border-gray-200 md:rounded-xl md:border-[1px] md:p-6 md:hover:bg-gray-50"
+        onClick={() => router.push(`/apply/${data.formId}/${data.id}`)}
+      >
+        <Flex
+          justifyContent="between"
+          alignItems="center"
+          gap={1}
+          className="w-full"
+        >
+          <Flex dir="col" alignItems="start" gap={1}>
+            <Body1 weight="bold">{data.name}</Body1>
+            <Body3 weight="semibold" className="text-gray-400">
+              {data.studentNumber}
+            </Body3>
           </Flex>
-        </Link>
+          <Badge
+            variant={
+              STATUS_TYPE[status].backgroundColor as
+                | 'positive'
+                | 'negative'
+                | 'neutral'
+            }
+            text={STATUS_TYPE[status].text}
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
