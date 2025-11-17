a886e213a04bcfb2f7a37e810d3da33bf5ad3576
keemsebeen <sebin0580@gmail.com>
2025-11-17 21:49:49 +0900

    chore: 불필요한 코드 정리

--- src/types/form.ts
+++ src/types/form.ts
@@ -2,20 +2,6 @@ export interface ManageFormProps {
   formData?: FormData;
 }
 
-export interface FormAnswer {
-  fieldId: string | number;
-  value: string | string[];
-}
-
-export interface ApplyData {
-  name: string;
-  studentNumber: string;
-  department: string;
-  email: string;
-  phoneNumber: string;
-  formAnswers: FormAnswer[];
-}
-
 export interface FormBlockData {
   formId: number;
   title: string;
