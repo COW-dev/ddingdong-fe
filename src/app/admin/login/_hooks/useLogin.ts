fb9e6e940fef8fa38ed2d320c1cba8bcb4dcc594
keemsebeen <sebin0580@gmail.com>
2025-11-30 19:50:39 +0900

    feat: 로그인 에러 처리

--- src/app/admin/login/_hooks/useLogin.ts
+++ src/app/admin/login/_hooks/useLogin.ts
@@ -7,6 +7,7 @@ import { toast } from 'react-hot-toast';
 import { useLoginMutation } from '@/app/_api/mutations/login';
 
 import { useCookie } from '../../../_api/useCookie';
+import { ApiError } from '@/app/_api/fetcher';
 
 export const useLogin = () => {
   const [id, setId] = useState('');
@@ -37,10 +38,23 @@ export const useLogin = () => {
     if (!id || !password) {
       return toast.error('아이디와 비밀번호를 모두 입력해주세요.');
     }
-    const { role, token } = await loginMutation({ id, password });
+    const { role, token } = await loginMutation(
+      { id, password },
+      {
+        onSuccess: () => {
+          router.push('/');
+        },
+        onError: (error: Error) => {
+          if (error instanceof ApiError) {
+            toast.error(error.message);
+          } else {
+            toast.error('로그인에 실패했습니다.');
+          }
+        },
+      },
+    );
     const authToken = token.split('Bearer ')[1];
     setCookies(authToken, role);
-    router.push('/');
   };
 
   return {
