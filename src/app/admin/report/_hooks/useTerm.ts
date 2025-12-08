b47e51e505e4bdf526f5b610d51207e96e8356ac
yougyung <youkyeongs@naver.com>
2025-12-08 20:53:13 +0900

    hotfix: currentTerm data 대응

--- src/app/admin/report/_hooks/useTerm.ts
+++ src/app/admin/report/_hooks/useTerm.ts
@@ -1,11 +1,12 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+// import { useSuspenseQuery } from '@tanstack/react-query';
 
-import { reportQueryOptions } from '@/app/_api/queries/report';
+// import { reportQueryOptions } from '@/app/_api/queries/report';
 
 export const useTerm = (term: number) => {
-  const { data: currentTerm } = useSuspenseQuery(
-    reportQueryOptions.currentTerm(),
-  );
+  // const { data: currentTerm } = useSuspenseQuery(
+  //   reportQueryOptions.currentTerm(),
+  // );
+  const currentTerm = { term: 15 };
 
   return {
     isClosed: term < currentTerm.term,
