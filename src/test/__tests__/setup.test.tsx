d5b2b7e948b211ca0850b4243048340602fac5a4
심유진 <syj11243@naver.com>
2026-01-08 07:15:36 +0900

    fix: screen import 경로 수정

--- src/test/__tests__/setup.test.tsx
+++ src/test/__tests__/setup.test.tsx
@@ -1,6 +1,7 @@
+import { screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 
-import { render, screen } from '@/test/utils';
+import { render } from '@/test/utils';
 
 describe('테스트 환경 설정 확인', () => {
   it('테스트 환경이 정상 작동해야 함', () => {
