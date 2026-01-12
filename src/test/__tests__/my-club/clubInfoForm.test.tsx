b4bcb61e5ab757ee7502ca851d2510a45113f248
Yehoon jang <168616991+JangYEhoon00@users.noreply.github.com>
2026-01-12 15:14:44 +0900

    test: ClubInfoForm 텍스트 표시 여부 확인
    ClubInfoForm 화면에 보여지는 텍스트 확인 테스트 코드 작성

---
+++ src/test/__tests__/my-club/clubInfoForm.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+
+import { ClubDetail } from '@/app/_api/types/club';
+import { ClubInfoForm } from '@/app/admin/my-club/_components/ClubInfoForm';
+
+const mockClub = {
+  //  type Props = {
+  //   club: ClubDetail;
+  //   isEditing: boolean;
+  //   onChange: (
+  //     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  //   ) => void;
+  //   onReset: (name: string) => void;
+  // };
+  leader: '김띵동',
+  phoneNumber: '010-1234-5678',
+  location: 's1234',
+  regularMeeting: '매주 수요일 18:00',
+} as ClubDetail;
+
+describe('ClubInfoForm', () => {
+  it('렌더링을 하면 동아리에 대한 정보를 표시한다.', () => {
+    const onChange = vi.fn();
+    const onReset = vi.fn();
+
+    render(
+      <ClubInfoForm
+        club={mockClub}
+        isEditing={false}
+        onChange={onChange}
+        onReset={onReset}
+      />,
+    );
+
+    expect(screen.getByText('김띵동')).toBeInTheDocument();
+    expect(screen.getByText('010-1234-5678')).toBeInTheDocument();
+    expect(screen.getByText('')).toBeInTheDocument();
+  });
+});
