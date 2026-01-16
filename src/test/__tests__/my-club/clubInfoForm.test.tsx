95f22a531d5d64fb25bb79282db911030e941e6f
Yehoon jang <rainer8191@gmail.com>
2026-01-16 14:28:07 +0900

    fix : 폼에 있는 모든 데이터 테스팅
    추가적으로 시나리오를 더 구체적으로 작성했습니다.

--- src/test/__tests__/my-club/clubInfoForm.test.tsx
+++ src/test/__tests__/my-club/clubInfoForm.test.tsx
@@ -2,16 +2,9 @@ import { screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 
-import { ClubDetail } from '@/app/_api/types/club';
 import { ClubInfoForm } from '@/app/admin/my-club/_components/ClubInfoForm';
 import { render } from '@/test/utils';
-
-const mockClub = {
-  leader: '김주장',
-  phoneNumber: '010-1234-5678',
-  location: 'S1234',
-  regularMeeting: '매주 수요일 18:00',
-} as ClubDetail;
+import { myClubMock as mockClub } from './my-clubMock';
 
 describe('ClubInfoForm 테스트', () => {
   const mockOnChange = vi.fn();
@@ -30,14 +23,19 @@ describe('ClubInfoForm 테스트', () => {
         onReset={mockOnReset}
       />,
     );
-
-    expect(screen.getByText('김주장')).toBeInTheDocument();
-    expect(screen.getByText('010-1234-5678')).toBeInTheDocument();
+    const texts = [
+      '김주장',
+      '010-1234-5678',
+      'S1234',
+      '2025.08.21~2025.09.04',
+    ];
+    texts.forEach((text) => {
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
   });
 
-  it('입력값을 변경할 때 onChange가 호출된다', async () => {
+  it('편집 모드일 경우 사용자가 입력값을 변경할 수 있다.', async () => {
     const user = userEvent.setup();
-
     render(
       <ClubInfoForm
         club={mockClub}
@@ -46,6 +44,7 @@ describe('ClubInfoForm 테스트', () => {
         onReset={mockOnReset}
       />,
     );
+
     const input = screen.getByDisplayValue('김주장');
     await user.clear(input);
     await user.type(input, '박띵동');
