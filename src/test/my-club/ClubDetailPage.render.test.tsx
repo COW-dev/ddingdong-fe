b75a4053685a643518f9bc30921574b2adf13fc5
Yehoon jang <rainer8191@gmail.com>
2026-01-16 15:31:23 +0900

    test : my-club페이지 렌더링 테스트

---
+++ src/test/my-club/ClubDetailPage.render.test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+
+import { render } from '@/test/utils';
+import { ClubDetailView } from '@/app/admin/my-club/_pages/ClubDetailClientPage';
+
+import { myClubMock } from './my-clubMock';
+
+describe('ClubDetailPage 테스트', () => {
+  it('페이지 렌더링 시 동아리 정보를 표시한다.', () => {
+    render(<ClubDetailView clubData={myClubMock} />);
+
+    const texts = [
+      '테스트 동아리',
+      '김주장',
+      '010-1234-5678',
+      'S1234',
+      '동아리 소개',
+      '동아리 활동',
+      '성실한 사람',
+    ];
+
+    texts.forEach((text) => {
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
+  });
+});
