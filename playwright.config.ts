bc356a2d6e2d90ea9de03c5f1bc653c39fd000ef
Yehoon jang <rainer8191@gmail.com>
2026-02-12 00:19:41 +0900

    [TEST] E2E테스트 초기 세팅

---
+++ playwright.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const isCi = !!process.env.CI;
+const baseURL = process.env.BASE_URL || 'http://localhost:3000';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: isCi,
+  workers: isCi ? 1 : undefined,
+  retries: isCi ? 2 : 0,
+  reporter: 'html',
+
+  use: {
+    baseURL: baseURL,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+
+  webServer: {
+    command: 'npm run dev',
+    url: baseURL,
+    reuseExistingServer: !isCi,
+  },
+});
