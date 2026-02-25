6781e923c05b31c120fa55fd06a268f96c232a02
Yehoon jang <rainer8191@gmail.com>
2026-02-25 11:16:55 +0900

    fix: 계산 버그 수정
    전월 대비한 날짜를 기준으로 계산되어 30일 31일 에 대해 날짜가 하루 앞당겨지거나 추가되는 현상이 발생했고 이를 setDate(1)을 먼저 호출해 이런 문제를 고쳤습니다.

--- src/app/admin/ranking/_hooks/getRankingDate.ts
+++ src/app/admin/ranking/_hooks/getRankingDate.ts
@@ -1,9 +1,10 @@
-export function useRankingDate() {
+export function getRankingDate() {
   const now = new Date();
   const currentYear = now.getFullYear();
   const currentMonth = now.getMonth() + 1;
 
   const lastMonthDate = new Date(now);
+  lastMonthDate.setDate(1);
   lastMonthDate.setMonth(now.getMonth() - 1);
   const lastMonthYear = lastMonthDate.getFullYear();
   const lastMonthMonth = lastMonthDate.getMonth() + 1;
