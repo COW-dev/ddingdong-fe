6107b50e3608d4dc4e63cfaad881a384e24cb6db
Yehoon jang <rainer8191@gmail.com>
2026-02-25 11:07:08 +0900

    feat: 로딩 컴포넌트 추가

---
+++ src/app/admin/ranking/_components/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from '@/components/loading/Loading';
+
+export default function RankingLoading() {
+  return (
+    <div className="flex w-full justify-center py-[3.2rem] md:py-[4.8rem]">
+      <Loading />
+    </div>
+  );
+}
