d8b5e2d9954dd14203e9290171e2e6313e303791
Yehoon jang <rainer8191@gmail.com>
2026-02-26 16:44:44 +0900

    refactor: 클라이언트 페이지 변경
    각 상위에 suspense가 필요하지 않아 클라이언트 페이지를 변경했습니다.

--- src/app/admin/ranking/page.tsx
+++ src/app/admin/ranking/page.tsx
@@ -1,17 +1,44 @@
-import { Suspense } from 'react';
+'use client';
 
-import { Metadata } from 'next';
+import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { Tabs, TabItem, Flex } from 'ddingdong-design-system';
 
-import RankingClientPage from './_pages/RankingClientPage';
+import { rankingQueryOptions } from '@/app/_api/queries/ranking';
 
-export const metadata: Metadata = {
-  title: '띵동 - 랭킹',
-};
+import { RankingHeader } from './_components/RankingHeader';
+import { RankingTable } from './_components/RankingTable';
+import { getRankingDate } from './_hooks/getRankingDate';
 
-export default function RankingPage() {
+export default function RankingClientPage() {
+  const { currentYear, currentMonth, lastMonthYear, lastMonthMonth } =
+    getRankingDate();
+
+  const { data: thisMonthRanking } = useSuspenseQuery(
+    rankingQueryOptions.adminFeedRanking(currentYear, currentMonth),
+  );
+
+  const { data: lastMonthRanking } = useQuery({
+    ...rankingQueryOptions.adminFeedRanking(lastMonthYear, lastMonthMonth),
+    enabled: true,
+  });
+
   return (
-    <Suspense>
-      <RankingClientPage />
-    </Suspense>
+    <Flex className="flex w-full flex-col py-[3.2rem] md:py-[4.8rem]">
+      <RankingHeader />
+      <Flex className="mt-8 w-full">
+        <Tabs defaultIndex={0}>
+          <TabItem label="이달의 랭킹">
+            <Flex className="mt-[1.6rem]">
+              <RankingTable data={thisMonthRanking} />
+            </Flex>
+          </TabItem>
+          <TabItem label="지난달 랭킹">
+            <Flex className="mt-[1.6rem]">
+              <RankingTable data={lastMonthRanking ?? []} />
+            </Flex>
+          </TabItem>
+        </Tabs>
+      </Flex>
+    </Flex>
   );
 }
