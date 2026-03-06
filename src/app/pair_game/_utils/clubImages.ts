f60f7ea779f01850710dbc0c49a67888c3b9f818
심유진 <syj11243@naver.com>
2026-03-06 10:14:27 +0900

    feat: 동아리 박람회 이벤트 제거

--- src/app/pair_game/_utils/clubImages.ts
+++
@@ -1,7 +0,0 @@
-import { CLUBS } from '../_constants/clubs';
-import { PAIR_GAME_PATH } from '../_constants/gameImages';
-
-export const getClubImageSrc = (clubId: number): string =>
-  `${PAIR_GAME_PATH}/clubs/${clubId}.webp`;
-
-export const CLUB_IMAGE_SRCS = CLUBS.map((c) => getClubImageSrc(c.id));
