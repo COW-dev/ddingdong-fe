4b448d3dcbeca48f82a13beb4d3cc1e9708e6217
keemsebeen <sebin0580@gmail.com>
2026-03-31 00:08:26 +0900

    chore: webp, png, svg 타입 선언

--- apps/web/global.d.ts
+++ apps/web/global.d.ts
@@ -1,1 +1,28 @@
 declare module '*.css';
+
+declare module '*.webp' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+  };
+  export default content;
+}
+
+declare module '*.png' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+  };
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+  };
+  export default content;
+}
